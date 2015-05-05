52ccfe32de9d5d44a68f9a37e970ad2175e51281
Kenji Morita <kennjimoritata@yahoo.co.jp>
2015-05-05 14:49:08 +0900

    【web pack】Vue.js(demo)外部requireの取り入れ方

--- main.js
+++ main.js
@@ -1,2 +1,12 @@
 var print = require("./print");
 print("Hello webpack");
+
+
+//vue.js
+var Vue = require('vue');
+var demo = new Vue({
+	el: "#demo",
+	data:{
+		message:"moritakenji!!!VueDebut!!"
+	}
+})
