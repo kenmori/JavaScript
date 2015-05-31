c18aa78722d1235d55fe9beb346f92d8b412a9bd
Kenji Morita <kennjimoritata@yahoo.co.jp>
2015-05-31 14:45:12 +0900

    initializeメソッドでnew演算子を使って初期化する際にBackboneModelから自動的に呼び出してもらう

--- js/backbone_main.js
+++ js/backbone_main.js
@@ -1,13 +1,21 @@
+// var Contact = Backbone.Model.extend({
+// 	defaults : {
+// 		firstName : '',
+// 		lastName : '',
+// 		email : ''
+// 	}
+// });
+// var contact = new Contact({
+// 	firstName : 'kenji',
+// 	lastName : 'morita',
+// 	email : 'kenji@example.com'
+// });
+// console.log(JSON.stringify(contact,null,2));//第2引数は出力される値のフィルタリングや加工のためのオプション、第3引数はインデント幅指定
+
+
 var Contact = Backbone.Model.extend({
-	defaults : {
-		firstName : '',
-		lastName : '',
-		email : ''
+	initialize : function(){
+		console.log('Contactが初期化されました');
 	}
 });
-var contact = new Contact({
-	firstName : 'Alice',
-	lastName : 'Henderson',
-	email : 'alice@example.com'
-});
-console.log(JSON.stringify(contact,null,2));
+var contact = new Contact();
