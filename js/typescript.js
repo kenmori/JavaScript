ab00c5d5a9c0f42a85946d4951495eabddda7315
Kenji Morita <kennjimoritata@yahoo.co.jp>
2015-06-06 12:53:45 +0900

    extendsで継承

--- js/typescript.js
+++ js/typescript.js
@@ -12,3 +12,31 @@ var morita = new man("morita");
 morita["greeting"]();
 var morim = new man("morim");
 morim["greeting"]();
+
+
+
+//extendsした場合の違い
+var __extends = (this && this.__extends) || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    __.prototype = b.prototype;
+    d.prototype = new __();
+};
+var man = (function () {
+    function man(name) {
+        this.name = name;
+    }
+    return man;
+})();
+var greetingMan = (function (_super) {
+    __extends(greetingMan, _super);
+    function greetingMan() {
+        _super.apply(this, arguments);
+    }
+    greetingMan.prototype.greeting = function () {
+        alert(this.name + "君");
+    };
+    return greetingMan;
+})(man);
+var morita = new greetingMan("morita");
+morita.greeting();
--- js/typescript.ts
+++ js/typescript.ts
@@ -1,12 +1,30 @@
-class man {
-	constructor(public name: string){}
-}
-function greeting(){
-	alert(this.name + "君");
-}
-man.prototype["greeting"] = greeting;
-var morita = new man("morita");
-morita["greeting"]();
+// class man {
+// 	constructor(public name: string){}
+// }
+// function greeting(){
+// 	alert(this.name + "君");
+// }
+// man.prototype["greeting"] = greeting;
+// var morita = new man("morita");
+// morita["greeting"]();
+//
+// var morim = new man("morim");
+// morim["greeting"]();
 
-var morim = new man("morim");
-morim["greeting"]();
+//上記だとgreeting機能が要らない場合無駄が増える
+//greetingのつづりを間違えてもコンパイルエラーにはならない
+//greeting　お引数の数や型を間違えてもコンパイルエラーにはならない
+//型に関する扱いが杜撰すぎる
+
+
+//上記の問題を解決
+class man {
+	constructor(public name : string){}
+}
+class greetingMan extends man{
+	greeting(){
+		alert(this.name + "君");
+	}
+}
+var morita = new greetingMan("morita");
+morita.greeting();
