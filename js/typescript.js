44bd9fa4faf33005b6517d7524edcfb2e1ed048b
Kenji Morita <kennjimoritata@yahoo.co.jp>
2015-06-06 20:55:17 +0900

    createElement関数で使用されている機能として、特殊化されたシグネチャ

--- js/typescript.js
+++ js/typescript.js
@@ -1,12 +1,34 @@
-var realPerson = (function () {
-    function realPerson() {
+//createElement関数で使用されている機能として、特殊化されたシグネチャがある。
+//これは文字列の引数の値によって返す型を変動させる機能がある
+var male = (function () {
+    function male() {
     }
-    realPerson.prototype.greeting = function () {
-        alert("Hello,I'm " + this.name + "and My age is" + this.age + ".");
+    male.prototype.sayMale = function () {
+        alert("俺は男だ");
     };
-    return realPerson;
+    return male;
 })();
-var a = new realPerson();
-a.name = "Morita";
-a.age = 34;
-a.greeting();
+var female = (function () {
+    function female() {
+    }
+    female.prototype.sayFemale = function () {
+        alert("俺は女だ");
+    };
+    return female;
+})();
+var builder = (function () {
+    function builder() {
+    }
+    builder.prototype.create = function (sex) {
+        if (sex == "male")
+            return new male();
+        else
+            return new female();
+    };
+    return builder;
+})();
+var a = new builder();
+var f = a.create("female");
+f.sayFemale();
+var m = a.create("male");
+m.sayMale();
--- js/typescript.ts
+++ js/typescript.ts
@@ -1,18 +1,29 @@
-interface abstractPerson{
-	name : string;
-	greeting(): void;
-}
-interface abstractPersonWithAge extends abstractPerson{
-	age: number;
-}
-class realPerson implements abstractPersonWithAge{
-	name: string;
-	age: number;
-	greeting(){
-		alert("Hello,I'm " + this.name + "and My age is" + this.age + ".");
+//createElement関数で使用されている機能として、特殊化されたシグネチャがある。
+//これは文字列の引数の値によって返す型を変動させる機能がある
+
+class male{
+	sayMale(){
+		alert("俺は男だ");
 	}
 }
-var a = new realPerson();
-a.name = "Morita";
-a.age = 34;
-a.greeting();
+class female{
+	sayFemale(){
+		alert("俺は女だ");
+	}
+}
+interface abstractPersonBuilder{
+	create(sex: "male"): male;
+	create(sex: "female"): female;
+	create(sex: "string"): any;
+}
+class builder implements abstractPersonBuilder{
+	create(sex: string): any{
+		if(sex == "male") return new male();
+		else return new female();
+	}
+}
+var a: abstractPersonBuilder = new builder();
+var f = a.create("female");
+f.sayFemale();
+var m = a.create("male");
+m.sayMale();
