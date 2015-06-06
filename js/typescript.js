25a8e956d86a1e7d2077dde19ebacdb99c08354e
Kenji Morita <kennjimoritata@yahoo.co.jp>
2015-06-06 11:17:00 +0900

    コンストラクターの引数でメンバーを宣言する

--- js/typescript.js
+++ js/typescript.js
@@ -1,25 +1,13 @@
-var cat = (function () {
-    function cat() {
-        alert("constructed");
-        this.name = "Unknown";
-        this.age = 1;
+// class cat {
+// 	public name = "Unknown";
+// }
+// var x = new cat();
+// alert(x.name);
+var man = (function () {
+    function man(firstName, lastName) {
+        this.name = lastName + firstName;
     }
-    cat.prototype.myau = function () { alert(this.name + 'は泣きました'); };
-    Object.defineProperty(cat.prototype, "age", {
-        get: function () { return this.localAge; },
-        set: function (value) {
-            if (value < 0)
-                throw "ageは負の数では無いはず";
-            this.localAge = value;
-        },
-        enumerable: true,
-        configurable: true
-    });
-    return cat;
+    return man;
 })();
-var x = new cat();
-x.name = "nya-chan";
-alert(x.name);
-x.myau();
-x.age = 5;
-alert(x.age);
+var x = new man("賢二", "森田");
+x.name[1];
--- js/typescript.ts
+++ js/typescript.ts
@@ -1,53 +1,46 @@
-//
-//
-//
 // class cat {
-// 	name : string;
-// 	myau(){alert(this.name + 'は泣きました')}
-// 	localAge : number;
-// 	get age(){return this.localAge;}
-// 	set age(value){
-// 		if(value < 0) throw "ageは負の数では無いはず";
-// 		this.localAge = value;
-// 	}
-// 	constructor(){
-// 		alert("constructed");
-// 		this.name = "Unknown";
-// 		this.age = 1;
-// 	}
+// 	public name = "Unknown";
 // }
 // var x = new cat();
-// x.name = "nya-chan";
 // alert(x.name);
-// x.myau();
-// x.age = 5;
-// alert(x.age);
+
+
+//###1
+// class cat {
+// 	public name: string;
+// 	public age: number;
+// 	constructor(name: string,age: number){
+// 		this.name = name;
+// 		this.age = age;
+// 	}
+// }
+// var x = new cat("Mee-kun",1);
+// alert(x.name + "は" + x.age + "歳");
+
+//###2
+//1と全く同じ結果を出すコンストラクター
+//コンストラクターにアクセシビリティのキーワードが付くと、その引数には同名のメンバーが存在し
+//そのメンバーに値が引き継がれることを意味する。
+//メンバーを宣言せずともメンバーが誕生し、コンストラクタでの代入が行われなくともそこに引数を経由して値が入る
 //
+// class cat {
+// 	constructor(public name: string,public age: number){
 //
-//アクセシビリティを付ける。このままだと
-//setを通らない直接の値代入が可能になる。x.localAge = -5;
-//privete:クラスの外からは利用できない。内部利用のみ(値の保存専用の変数)
-//public: クラスの内外から無制限で利用できる(外部からの受付窓口)
+// 	}
+// }
+// var x = new cat("Mee-Kun",1);
+// alert(x.name + "は" + x.age + "歳");
+
+
+//上記を使うには制約がある
+//引数名とメンバーが同じ
+//値をコンスタラクターで加工することがない
 //
-class cat {
-	public name : string;
-	public myau(){alert(this.name + 'は泣きました')}
-	private localAge : number;
-	public get age(){return this.localAge;}
-	public set age(value){
-		if(value < 0) throw "ageは負の数では無いはず";
-		this.localAge = value;
-	}
-	public constructor(){
-		alert("constructed");
-		this.name = "Unknown";
-		this.age = 1;
+class man {
+	public name: string;
+	constructor(firstName: string, lastName: string){
+		this.name = lastName + firstName;
 	}
 }
-var x = new cat();
-x.name = "nya-chan";
-alert(x.name);
-x.myau();
-//x.localAge = -5//書き換えようとするとコンパイルエラーになる
-x.age = 5;
-alert(x.age);
+var x = new man("賢二","森田");
+x.name[1];
