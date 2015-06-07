9b1aa05e5d36a48d7fef736bf9981d5910dc0c08
Kenji Morita <kennjimoritata@yahoo.co.jp>
2015-06-07 12:10:32 +0900

    列挙型

--- js/typescript.js
+++ js/typescript.js
@@ -1,25 +1,6 @@
-setTimeout(function () {
-    alert("1秒が経過しました");
-}, 1000);
-function mainFunc(f) {
-    alert(f(1, 2));
-}
-mainFunc(function (a, b) { return a + b; });
-function makeName(f) {
-    alert(f("battle", "star"));
-}
-makeName(function (a, b) { return a + b; });
-function makeName(f) {
-    alert(f("はい"));
-}
-makeName(function (a) { return a + a + a; });
-function makeName(f) {
-    alert(f("はい"));
-}
-makeName(function (a) { return a + a + a; });
-var a = (function () {
-    function a() {
-        this.b = "I'm class a";
-    }
-    return a;
-})();
+//列挙型
+var win = true;
+if (win)
+    alert("赤組の勝利です");
+else
+    alert("白組の勝利です");
--- js/typescript.ts
+++ js/typescript.ts
@@ -1,36 +1,10 @@
-//アロー関数(匿名関数の置き換え)
-//
-//
-setTimeout(() => {
-	alert("1秒が経過しました");
-},1000)
+//列挙型
 
-//右側に戻り値の型を書く
-function mainFunc(f:(a:number,b: number) => number){
-	alert(f(1,2));
-}
-mainFunc((a,b) => {return a + b;})
-//戻り値が無い場合は()=>voidのように記述する
+// var win:string = "紅組";//ここが「赤組」となると白組が勝利してします。関係の無い文字列が入力されてしまうのが問題
+// if(win == "紅組") alert("紅組の勝利です");
+// else alert("白組の勝利です");
+//自由度の高い文字列ではなくonかoffかのbooleanをつかう
 
-//アロー関数はreturn文一つきりの時
-function makeName(f:(a:string,b:string) => string){
-	alert(f("battle","star"));
-}
-makeName((a,b)=> a+ b);
-//returnと{}を省略できる
-
-//引数が一つの時、アロー関数は引数の小括弧をはずせる
-function makeName(f:(a: string) => string){
-	alert(f("はい"));
-}
-makeName((a) => a+ a+ a);
-
-//外したパターン
-function makeName(f: (a: string) => string){
-	alert(f("はい"));
-}
-makeName(a => a + a + a);
-
-class a{
-	b = "I'm class a";
-}
+var win:boolean = true;
+if(win) alert("赤組の勝利です");
+else alert("白組の勝利です");
