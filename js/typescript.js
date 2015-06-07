af3654201223f28e8f6d9a972f5302e6b33b8e42
Kenji Morita <kennjimoritata@yahoo.co.jp>
2015-06-07 12:18:28 +0900

    enumキーワード

--- js/typescript.js
+++ js/typescript.js
@@ -1,6 +1,11 @@
 //列挙型
-var win = true;
-if (win)
-    alert("赤組の勝利です");
+var team;
+(function (team) {
+    team[team["紅組"] = 0] = "紅組";
+    team[team["白組"] = 1] = "白組";
+})(team || (team = {}));
+var win = team.紅組;
+if (win == team.紅組)
+    alert("紅組の勝利です");
 else
-    alert("白組の勝利です");
+    alert("白組の勝利です!");
--- js/typescript.ts
+++ js/typescript.ts
@@ -5,6 +5,16 @@
 // else alert("白組の勝利です");
 //自由度の高い文字列ではなくonかoffかのbooleanをつかう
 
-var win:boolean = true;
-if(win) alert("赤組の勝利です");
-else alert("白組の勝利です");
+// var win:boolean = true;
+// if(win) alert("紅組の勝利です");
+// else alert("白組の勝利です");
+//これではtrueと文字列は無関係
+
+enum team {
+	紅組,白組
+}
+var win: team = team.紅組;
+if(win == team.紅組) alert("紅組の勝利です");
+else alert("白組の勝利です!");
+
+//これで変数の値がnullやundefinedの場合の誤判定やany型などを使用して型システムをバイパスしてしまう場合の問題を除けば対処できた
