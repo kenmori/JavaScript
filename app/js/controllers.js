c74fc4e006b9de4886439f497a548a6a72cee96b
Kenji Morita <kennjimoritata@yahoo.co.jp>
2015-06-08 19:57:13 +0900

    tutorial-step1

---
+++ app/js/controllers.js
@@ -0,0 +1,13 @@
+$scope.phones = [
+	{	'name' : 'Nexus S',
+		'snippet' : 'Fast just got faseter with Nexus S.'
+	},
+	{
+		'name' : 'Motorola XOOM? width Wi-Fi',
+		'snippet' : 'The Next,Next Generation tablet.'
+	},
+	{
+		'name' : 'MOTOROLA XOOM?',
+		'snippet' : 'The Next,Next Genaration tablet.'
+	}
+];
