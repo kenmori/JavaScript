1a35c9a5074fdc605dc60fff99a7f0b3dde78ff5
Kenji Morita <kennjimoritata@yahoo.co.jp>
2015-06-28 06:41:16 +0900

    how to generic

--- js/typescript.js
+++ js/typescript.js
@@ -1,17 +1,48 @@
-function x(a, b) {
-    if (b === void 0) { b = 100; }
-    return a + b;
+function a(x) {
+    alert(x);
 }
-alert(x(100));
-function x() {
-    var a = [];
-    for (var _i = 0; _i < arguments.length; _i++) {
-        a[_i - 0] = arguments[_i];
-    }
-    var sum = 0;
-    for (var i = 0; i < a.length; i++) {
-        sum += a[i];
-    }
-    return sum;
+a("eee");
+a(9);
+function b(x) {
+    alert(x);
 }
-alert(x(100, 200, 300, 400, 500));
+;
+b("fafa");
+b(9);
+var MyPerson = (function () {
+    function MyPerson() {
+    }
+    return MyPerson;
+})();
+function c(t, u) {
+    alert(t);
+    alert(u);
+}
+c('finger', 1);
+function d(t) {
+    return t;
+}
+alert(d(1234));
+function e() {
+    var x = null;
+    alert(x);
+}
+e();
+function g(x) {
+    alert(x instanceof Date);
+}
+g("new Date()");
+g(new Date());
+function h(x) {
+    alert(x instanceof Date);
+}
+h("new Date()");
+h(new Date());
+function j(x) {
+    alert(x);
+    return x;
+}
+function k(x) {
+    var y = j(x);
+}
+k(new Date());
