6c1ae5b9e362f05b9f0c915e2838e989cec3ea8d
zheng <zheng@sansan.com>
2017-07-24 20:34:58 +0900

    :up: プランのモデル追加

---
+++ app/models/plan.rb
@@ -0,0 +1,15 @@
+class Plan < ApplicationRecord
+  belongs_to :key_result
+
+  enum status: {
+    not_yet: 1,
+    doing: 2,
+    done: 3
+  }
+
+  validates :status, presence: true, inclusion: {in: self.statuses.keys}
+  # TODO:
+  # 一旦プランで入力可能な文字数を500に設定する。
+  validates :description, presence: true, length: {maximun: 500}
+
+end
