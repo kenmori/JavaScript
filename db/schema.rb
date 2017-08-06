96f0da7fd7b243dc2857835fe3220e61fb7fd961
zheng <zheng@sansan.com>
2017-08-06 23:11:09 +0900

    :wrench: テストデータ作成スクリプト修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,28 +10,62 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170723102341) do
+ActiveRecord::Schema.define(version: 20170802134252) do
+
+  create_table "concerned_people", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "key_result_id", null: false
+    t.integer "member_id", null: false
+    t.integer "role", null: false
+  end
+
+  create_table "groups", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "organization_id", null: false
+    t.string "name", null: false
+    t.integer "owner_id", null: false
+  end
 
   create_table "key_results", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "name", null: false
     t.integer "objective_id", null: false
+    t.integer "owner_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "objective_owners", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.integer "objective_id", null: false
-    t.string "name"
+  create_table "members", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "group_id", null: false
+    t.integer "user_id", null: false
+    t.index ["group_id", "user_id"], name: "index_members_on_group_id_and_user_id", unique: true
   end
 
   create_table "objectives", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "name", null: false
     t.string "description"
     t.integer "parent_objective_id"
+    t.integer "owner_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "organizations", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "name", null: false
+    t.string "postal_code"
+    t.string "address"
+    t.string "phone_number"
+  end
+
+  create_table "owners", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "kind", null: false
   end
 
   create_table "plans", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -40,7 +74,15 @@ ActiveRecord::Schema.define(version: 20170723102341) do
     t.integer "key_result_id", null: false
     t.string "description", null: false
     t.integer "status", default: 1, null: false
+    t.integer "concerned_person_id", null: false
     t.date "deadline"
+  end
+
+  create_table "roles", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "member_id", null: false
+    t.integer "kind", null: false
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -62,6 +104,7 @@ ActiveRecord::Schema.define(version: 20170723102341) do
     t.integer "failed_attempts", default: 0, null: false
     t.string "unlock_token"
     t.datetime "locked_at"
+    t.integer "owner_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,16 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+user_owner = Owner.create(kind: 1)
+user = User.new(name: 'test', email: 'test@example.com', password: 'testtest', owner_id: user_owner.id)
+user.skip_confirmation!
+user.save
 
-objective1 = Objective.create(name: '改善系タスクにより主要KPIをX%向上させる', description: '主要KPIに注力する')
+organization = Organization.create(name: 'sansan')
+group_owner = Owner.create(kind: 2)
+group = organization.groups.create(name: 'company', owner_id: group_owner.id)
+
+group.members.create(user_id: user.id)
+
+objective1 = group_owner.objectives.create(name: '改善系タスクにより主要KPIをX%向上させる', description: '主要KPIに注力する')
 objective1.child_objectives.create(name: '全体検索の速度改善でDAUあたりの利用回数15%改善', description: 'クエリチューニングなどを開発により実施する')
 objective1.key_results.create(name: 'KeyResult1')
 objective1.key_results.create(name: 'KeyResult2')
 
-objective2 = Objective.create(name: 'ビジネス成長に寄与する機能を開発する', description: '要検討')
+objective2 = group_owner.objectives.create(name: 'ビジネス成長に寄与する機能を開発する', description: '要検討')
 objective2.child_objectives.create(name: '×××をリリース', description: '')
 objective2.child_objectives.create(name: '◯◯◯リリース', description: '')
 
-user = User.new(name: 'test', email: 'test@example.com', password: 'testtest')
-user.skip_confirmation!
-user.save
+
