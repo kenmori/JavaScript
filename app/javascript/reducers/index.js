0d4d4d368c4f89070501894dfdb41313a71bfab4
rockwillj <rockwillj@gmail.com>
2017-08-30 23:55:15 +0900

    Modify: ユーザーに関する API (一覧取得、追加、更新、削除) のレスポンスを state に反映する処理を実装

--- app/javascript/reducers/index.js
+++ app/javascript/reducers/index.js
@@ -1,11 +1,13 @@
 import { combineReducers } from 'redux';
 import keyResults from './keyReuslts';
 import objectives from './objectives';
+import users from './users';
 import dialogs from './dialogs';
 
 const reducers = combineReducers({
   keyResults,
   objectives,
+  users,
   dialogs
 });
 
---
+++ app/javascript/reducers/users.js
@@ -0,0 +1,24 @@
+import { fromJS } from 'immutable';
+import { handleActions } from 'redux-actions';
+import ActionTypes from '../constants/actionTypes';
+
+export default handleActions({
+    [ActionTypes.FETCHED_USERS]: (state, { payload }) => {
+      return state.merge(payload.users);
+    },
+    [ActionTypes.ADDED_USER]: (state, { payload }) => (
+      state.push(payload.user)
+    ),
+    [ActionTypes.UPDATED_USER]: (state, { payload }) => {
+      return state.set(state.findIndex((user) => {
+        return user.get('id') === payload.user.get('id');
+      }), payload.user);
+    },
+    [ActionTypes.REMOVED_USER]: (state, { payload }) => {
+      return state.filter((user) => {
+        return user.get('id') !== payload.id;
+      });
+    },
+  },
+  fromJS([])
+);
