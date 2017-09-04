4e5de61a6a67767b6471b076c19612558446cc7b
rockwillj <rockwillj@gmail.com>
2017-09-04 18:48:00 +0900

    :bug: ユーザー追加時や削除時に画面が更新されない問題を修正

--- app/javascript/sagas/users.js
+++ app/javascript/sagas/users.js
@@ -2,6 +2,7 @@ import { all, call, put, takeLatest } from 'redux-saga/effects';
 import API from '../utils/api';
 import userActions from '../actions/users';
 import actionTypes from '../constants/actionTypes';
+import history from '../utils/history';
 
 function* fetchUsers() {
   const result = yield call(API.get, '/users');
@@ -11,16 +12,19 @@ function* fetchUsers() {
 function* addUser({ payload }) {
   const result = yield call(API.post, '/users', { user: payload.user });
   yield put(userActions.addedUser(result.get('user')));
+  yield call(history.go); //react-routerのバグのためここで無理やり画面を再読み込みさせている。今後、改修されれば削除する。
 }
 
 function* updateUser({ payload }) {
   const result = yield call(API.put, '/users/' + payload.user.id, { user: payload.user });
   yield put(userActions.updatedUser(result.get('user')));
+  yield call(history.go); //react-routerのバグのためここで無理やり画面を再読み込みさせている。今後、改修されれば削除する。
 }
 
 function* removeUser({ payload }) {
   yield call(API.delete, '/users/' + payload.id);
   yield put(userActions.removedUser(payload.id));
+  yield call(history.go); //react-routerのバグのためここで無理やり画面を再読み込みさせている。今後、改修されれば削除する。
 }
 
 export function* userSagas() {
