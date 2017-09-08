b4fb18bb80b0dfea51a6826c1a93669a078ceecf
rockwillj <rockwillj@gmail.com>
2017-09-08 15:34:59 +0900

    :sparkles: アカウント設定タブの実装 (ロジックは未実装)

--- app/javascript/components/SettingsPage.jsx
+++ app/javascript/components/SettingsPage.jsx
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import { Tab } from 'semantic-ui-react';
 import MenuBar from '../containers/MenuBar';
+import AccountSettingTab from '../containers/AccountSettingTab'
 import UserSettingTab from '../containers/UserSettingTab'
 
 class SettingsPage extends Component {
   get panes() {
     return ([
-      { menuItem: 'アカウント', render: () => <Tab.Pane attached={false}>Tab 1 Content</Tab.Pane> },
+      { menuItem: 'アカウント', render: () => <AccountSettingTab/> },
       { menuItem: 'ユーザー', render: () => <UserSettingTab/> },
       { menuItem: 'OKR', render: () => <Tab.Pane attached={false}>Tab 3 Content</Tab.Pane> },
     ]);
---
+++ app/javascript/containers/AccountSettingTab.jsx
@@ -0,0 +1,16 @@
+import AccountSettingTab from '../components/AccountSettingTab';
+import { connect } from 'react-redux';
+import userActions from '../actions/users';
+
+const mapStateToProps = (state) => {
+  return {};
+};
+
+const mapDispatchToProps = dispatch => {
+  return {};
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(AccountSettingTab);
