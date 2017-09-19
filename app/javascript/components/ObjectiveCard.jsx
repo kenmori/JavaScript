f5427cf0e197b0b42ac743c26d1f40bf3d03fe51
shinohara <shinohara@sansan.com>
2017-09-19 21:03:25 +0900

    :bug:importの記述に間違いがってprecompileに失敗したため修正した。

--- app/javascript/components/ObjectiveCard.jsx
+++ app/javascript/components/ObjectiveCard.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'React';
+import React, { Component } from 'react';
 import { Card, Icon } from 'semantic-ui-react';
 
 export default class ObjectiveCard extends Component {
