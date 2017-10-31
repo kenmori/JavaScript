b242ed4d165f6a9a0e2119d2ebb53ef1f4bf8454
rockwillj <rockwillj@gmail.com>
2017-10-31 19:40:30 +0900

    :wrench: メール送信に失敗した場合はエラー扱いにする

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,7 +32,7 @@ Rails.application.configure do
   config.read_encrypted_secrets = true
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  # config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
 
