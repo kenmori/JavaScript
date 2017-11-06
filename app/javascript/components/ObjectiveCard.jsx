8313487d40de4402e6482a0b47770deb1f55eca7
rockwillj <rockwillj@gmail.com>
2017-11-06 19:09:21 +0900

    Modify: ObjectiveCard に Key Result 数ではなく Key Result 名一覧を表示する

--- app/javascript/components/ObjectiveCard.jsx
+++ app/javascript/components/ObjectiveCard.jsx
@@ -1,16 +1,32 @@
 import React, { Component } from 'react';
-import { Card, Icon } from 'semantic-ui-react';
+import { Card, Icon, List } from 'semantic-ui-react';
 
 export default class ObjectiveCard extends Component {
+  generateKeyResultList = objective => {
+    const keyResults = objective.get('keyResults');
+    if (!keyResults || keyResults.isEmpty()) {
+      return <Card.Content description='Key Result はありません'/>;
+    }
+    return (
+      <Card.Content>
+        <List bulleted>
+          {keyResults.map(keyResult =>
+            <List.Item key={keyResult.get('id')}>{keyResult.get('name')}</List.Item>
+          )}
+        </List>
+      </Card.Content>
+    );
+  };
+
   render() {
     const objective = this.props.objective;
-    if(!objective) {
-      return <div></div>;
+    if (!objective) {
+      return null;
     }
     return (
       <Card key={objective.get('id')}>
         <Card.Content header={objective.get('name')}/>
-        <Card.Content description={`KeyResults: ${objective.get('keyResults').size}`}/>
+        {this.generateKeyResultList(objective)}
         <Card.Content extra>
           <Icon link name='plus' onClick={() => this.props.openKeyResultFormModal(objective)}/>
           <Icon link name='trash' onClick={() => this.props.removeObjective(objective.get('id'))}/>
