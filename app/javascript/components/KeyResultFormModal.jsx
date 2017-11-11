c16b4a629a7575278eda6fffa35e3def6d028cdb
ikumi.toshihiko <ikumit.org@gmail.com>
2017-11-11 22:51:35 +0900

    関係者セレクトボックス追加

--- app/javascript/components/KeyResultFormModal.jsx
+++ app/javascript/components/KeyResultFormModal.jsx
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
-import { Button, Form, Input, Modal, Dropdown } from 'semantic-ui-react';
+import { Button, Form, Input, Modal, Dropdown, Icon } from 'semantic-ui-react';
 import DatePicker from 'react-datepicker';
 import moment from 'moment';
 
 class KeyResultFormModal extends Component {
   constructor(props) {
     super(props);
-    this.state = {expiredDate: moment()}
+    this.state = {
+      expiredDate: moment(),
+      participants: [null]
+    }
   }
 
   usersOption(users) {
@@ -21,6 +24,34 @@ class KeyResultFormModal extends Component {
     this.setState({expiredDate: date})
   }
 
+  addParticipants(evt, { value }) {
+    const participants = this.state.participants;
+    participants[participants.length - 1] = value;
+    participants.push(null);
+
+    this.setState({
+      participants: participants
+    })
+  }
+
+  removeParticipants(clickedId) {
+    this.setState({
+      participants: this.state.participants.filter( id => id !== clickedId)
+    })
+  }
+
+  participantList(options, onChangeHandler, onCloseHandler) {
+    const list = this.state.participants.map((id, idx) => {
+      const icon = idx !== 0 && id !== null && <Icon name="close" className="participants__close" onClick={() => {onCloseHandler(id)}} />
+      return <div key={idx} className="participants__item">
+              <Dropdown selection value={id} options={options} onChange={onChangeHandler}/>
+              {icon}
+             </div>
+    })
+
+    return <div className="participants">{list}</div>;
+  }
+
   add() {
     const keyResult = {
       name: this.nameInput.inputRef.value,
@@ -29,6 +60,7 @@ class KeyResultFormModal extends Component {
       targetValue: this.targetInput.inputRef.value,
       valueUnit: this.unitInput.inputRef.value,
       expiredDate: this.state.expiredDate.format(),
+      members: this.state.participants.filter(Boolean)
     };
     this.props.addKeyResult(keyResult);
     this.nameInput.inputRef.value = '';
@@ -38,7 +70,10 @@ class KeyResultFormModal extends Component {
   componentWillReceiveProps(nextProps, currentProps) {
     const willClose = nextProps.isOpen !== currentProps.isOpen && !nextProps.isOpen;
     if (willClose) {
-      this.setState({expiredDate: moment()});
+      this.setState({
+        expiredDate: moment(),
+        participants: [null]
+      });
     }
   }
 
@@ -47,7 +82,7 @@ class KeyResultFormModal extends Component {
       return null;
     }
     return (
-      <Modal open={this.props.isOpen}>
+      <Modal open={this.props.isOpen} className="key-result-form-modal">
         <Modal.Header>
           {this.props.objective && this.props.objective.get('name')} の KeyResult を作成する
         </Modal.Header>
@@ -83,6 +118,12 @@ class KeyResultFormModal extends Component {
               <Form.Field>
                 <label>責任者</label>
                 <Dropdown selection options={this.usersOption(this.props.users)} ref={node => {this.ownerSelect = node;}}/>
+              </Form.Field>
+            </Form.Group>
+            <Form.Group>
+              <Form.Field>
+                <label>関係者</label>
+                {this.participantList(this.usersOption(this.props.users), this.addParticipants.bind(this), this.removeParticipants.bind(this))}
               </Form.Field>
             </Form.Group>
           </Form>
