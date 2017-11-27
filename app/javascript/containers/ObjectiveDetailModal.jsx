babd93a11658b9bb9d026bebe68a4cd090eb6a38
rockwillj <rockwillj@gmail.com>
2017-11-27 17:45:44 +0900

    Fix: OKR マップから他ユーザーが保持する Objective の OKR 編集画面が開けない (他ユーザーの Objective が state.objectives に存在しないのが原因。暫定対応として再帰的に childObjectives を探索するように修正)

--- app/javascript/containers/ObjectiveDetailModal.jsx
+++ app/javascript/containers/ObjectiveDetailModal.jsx
@@ -5,9 +5,26 @@ import keyResultActions from '../actions/keyResults';
 import dialogActions from '../actions/dialogs';
 
 const mapStateToProps = (state) => {
+  function findObjective(objectives, id) {
+    let objective = undefined;
+    objectives.forEach(item => {
+      if (item.get('id') === id) {
+        objective = item;
+        return false;
+      }
+      if (item.get('childObjectives')) {
+        objective = findObjective(item.get('childObjectives'), id);
+        if (objective) {
+          return false;
+        }
+      }
+    });
+    return objective;
+  }
+
   return {
     isOpen: state.dialogs.getIn(['objectiveDetail', 'isOpen']),
-    objective: state.objectives.find((objective) => { return objective.get('id') == state.dialogs.getIn(['objectiveDetail', 'objectiveId'])}),
+    objective: findObjective(state.objectives, state.dialogs.getIn(['objectiveDetail', 'objectiveId'])),
     users: state.users,
   };
 };
