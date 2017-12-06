ce0e885c138017ec79434c8eca55b802d1f7f4fd
rockwillj <rockwillj@gmail.com>
2017-12-06 17:19:01 +0900

    Modify: OKR Box で進捗率0%の KR も表示するため、各 KR の value と endAngle, minAngle を調整するロジックを実装

--- app/javascript/components/OkrPieChart.jsx
+++ app/javascript/components/OkrPieChart.jsx
@@ -5,33 +5,45 @@ import PropTypes from 'prop-types';
 class OkrPieChart extends Component {
 
   getPieChartProperties(objective) {
+    const startAngle = 90;
+    let endAngle = startAngle;
+    const minAngle = 10;
+
+    const data = objective.get('keyResults').map(keyResult => {
+      if (keyResult.get('progressRate') === 0) {
+        endAngle -= minAngle; // 進捗率0%の KR は minAngle 幅で表示されるためその分の幅を追加する
+      }
+      return {
+        name: keyResult.get('name'),
+        value: Math.max(0.1, keyResult.get('progressRate')), // 進捗率0%の KR を表示するため nonzero の値を指定する
+      }
+    });
+
     return {
-      data: objective.get('keyResults').map(keyResult => (
-        {
-          name: keyResult.get('name'),
-          value: Math.round(keyResult.get('progressRate') / objective.get('keyResults').size),
-        }
-      )).toArray(),
-      endAngle: 90 - Math.round(objective.get('progressRate') / 100 * 360),
+      data: data.toArray(),
+      startAngle: startAngle,
+      endAngle: endAngle - Math.round(objective.get('progressRate') / 100 * 360),
+      minAngle: minAngle,
       label: `${this.props.objective.get('progressRate')}%`,
     };
   }
 
   getPieChart = (objective) => {
-    const { data, endAngle, label } = this.getPieChartProperties(objective);
+    const { data, startAngle, endAngle, minAngle, label } = this.getPieChartProperties(objective);
     return (
       <PieChart width={160} height={160}>
         <Pie data={data}
              dataKey="value"
-             startAngle={90}
+             startAngle={startAngle} // 時計回りのアニメーションにするため startAngle/endAngle の値を指定する
              endAngle={endAngle}
+             minAngle={minAngle}
              innerRadius={50}
              outerRadius={70}
              fill="lightgray"
              paddingAngle={2}>
           <Label value={label} position="center" fill="indianred" />
         </Pie>
-        <Tooltip formatter={value => `${value}%`} />
+        <Tooltip formatter={value => `${Math.round(value)}%`} />
       </PieChart>
     );
   }
