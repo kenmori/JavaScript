bbd13cb3680f976e44833dfb57796fe281a7333b
ikumi.toshihiko <ikumit.org@gmail.com>
2017-12-21 08:42:39 +0900

    組織名が切り替わらないバグ修正

--- app/javascript/reducers/organizations.js
+++ app/javascript/reducers/organizations.js
@@ -3,18 +3,13 @@ import { handleActions } from 'redux-actions';
 import ActionTypes from '../constants/actionTypes';
 import gon from '../utils/gon';
 
-const organizations = gon.get('organizations');
-const initialSelectedOrganization = organizations ? organizations.get(0) : fromJS({});
 const initialState = fromJS({
-  selected: fromJS({
-    id: initialSelectedOrganization.get('id'),
-    name: initialSelectedOrganization.get('name'),
-  }),
-  list: organizations
+  selected: gon.get('organization'),
+  list: gon.get('organizations')
 });
 
 export default handleActions({
-  [ActionTypes.UPDATE_CURRENT_ORGANIZATION_ID]: (state, { payload }) => console.log(111, payload) || (
+  [ActionTypes.UPDATE_CURRENT_ORGANIZATION_ID]: (state, { payload }) => (
     state
   ),
 }, initialState);
