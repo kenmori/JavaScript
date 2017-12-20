b90b15558fb5b82f2a68ead7442ed9e5b8fa337d
shinot <shinot8324@gmail.com>
2017-12-20 22:58:44 +0900

    Modify: objectives, keyResultsの変更がマップとリストに伝播するようにした。

--- app/javascript/reducers/keyResults.js
+++ app/javascript/reducers/keyResults.js
@@ -6,16 +6,16 @@ export default handleActions({
     [ActionTypes.FETCHED_KEY_RESULTS]: (state, { payload }) => {
       return state.merge(payload.keyResults);
     },
-    [ActionTypes.ADDED_KEY_RESULT]: (state, { payload }) => (
+    [ActionTypes.ADDED_KEY_RESULT]: (state, { payload }) => {
       state.push(payload.keyResult)
-    ),
+    },
     [ActionTypes.UPDATED_KEY_RESULT]: (state, { payload }) => {
       return state.set(state.findIndex((keyResult) => {
-        return keyResult.get('id') === payload.keyResult.get('id');
+        return keyResult.get('id') === payload.getIn(['keyResult', 'id']);
       }), payload.keyResult);
     },
     [ActionTypes.REMOVED_KEY_RESULT]: (state, { payload }) => {
-      return state.filter(item => item.get('id') !== payload.keyResult.get('id'));
+      return state.filter(item => item.get('id') !== payload.getIn(['keyResult', 'id']));
     },
   },
   fromJS([])
--- app/javascript/reducers/objectives.js
+++ app/javascript/reducers/objectives.js
@@ -3,8 +3,8 @@ import { handleActions } from 'redux-actions';
 import ActionTypes from '../constants/actionTypes';
 import gon from '../utils/gon';
 
-
 const keyResultMethod = {
+  add: (keyResults, newKeyResult) => (keyResults.push(newKeyResult)),
   update: (keyResults, newKeyResult) => {
     return keyResults.map(keyResult => {
       if (keyResult.get('id') === newKeyResult.get('id')) {
@@ -49,6 +49,9 @@ export default handleActions({
           return childObjective.get('id') !== payload.id;
         }));
       });
+    },
+    [ActionTypes.ADDED_KEY_RESULT]: (state, { payload }) => {
+      return rebuildKeyResult(keyResultMethod.add, state, payload);
     },
     [ActionTypes.REMOVED_KEY_RESULT]: (state, { payload }) => {
       return rebuildKeyResult(keyResultMethod.remove, state, payload);
