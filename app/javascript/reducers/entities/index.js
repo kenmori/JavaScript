5844e6316a0915fcb7a0d03424c3a1a6a6f6018c
shinot <shinot8324@gmail.com>
2017-12-20 21:35:45 +0900

    Fix: KeyResultのreducerをentitiesディレクトリに移動させてListからMapへ変更した。あくまで参照用とするようにした。

---
+++ app/javascript/reducers/entities/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+import keyResults from './keyResults';
+
+const reducers = combineReducers({
+  keyResults,
+});
+
+export default reducers;
---
+++ app/javascript/reducers/entities/keyResults.js
@@ -0,0 +1,19 @@
+import { Map } from 'immutable';
+import { handleActions } from 'redux-actions';
+import ActionTypes from '../../constants/actionTypes';
+
+function merge(state, { payload }) {
+  const keyResult = payload.get('keyResult');
+  return state.merge(Map({[keyResult.get('id')]: keyResult}));
+}
+
+export default handleActions({
+    [ActionTypes.ADDED_KEY_RESULT]: merge,
+    [ActionTypes.UPDATED_KEY_RESULT]: merge,
+    [ActionTypes.REMOVED_KEY_RESULT]: (state, { payload }) => {
+      return state.delete(payload.get('id'));
+    },
+  },
+  Map()
+)
+
