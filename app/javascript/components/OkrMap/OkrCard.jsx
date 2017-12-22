f5179f9fe7a7d60ad21c0095f64b741cdbbcc929
rockwillj <rockwillj@gmail.com>
2017-12-22 16:42:02 +0900

    Remove: 子がいる場合に点線のパスを描画していた処理を削除 (子が描画されるようになったため)

--- app/javascript/components/OkrMap/OkrCard.jsx
+++ app/javascript/components/OkrMap/OkrCard.jsx
@@ -1,32 +1,10 @@
 import React, { Component } from 'react';
-import { findDOMNode } from 'react-dom';
 import PropTypes from 'prop-types';
 import { Card, Icon, List } from 'semantic-ui-react';
 import Avatar from '../../containers/Avatar';
 import moment from 'moment';
 
 class OkrCard extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { width: 0, height: 0, };
-  }
-
-  componentDidMount() {
-    this.setWidthAndHeight();
-  }
-
-  componentDidUpdate(prevProps, _prevState) {
-    if (prevProps !== this.props) {
-      // コンポーネント描画後の width, height を取得するためこのタイミングで呼び出す
-      this.setWidthAndHeight();
-    }
-  }
-
-  setWidthAndHeight = () => {
-    const card = findDOMNode(this.refs.card);
-    this.setState({ width: card.offsetWidth, height: card.offsetHeight, });
-  }
-
   generateKeyResultList(objective) {
     const keyResults = objective.get('keyResults');
     return (
@@ -55,24 +33,6 @@ class OkrCard extends Component {
     );
   }
 
-  pathSvg = (parentOrNot, drawOrNot) => {
-    if (drawOrNot) {
-      const top = parentOrNot ? '-22' : this.state.height;
-      const x = Math.round(this.state.width / 2);
-      return (
-        <svg width={this.state.width} height='22' style={{ position: 'absolute', top: top }}>
-          <line
-            x1={x} y1='0'
-            x2={x} y2='22'
-            stroke='silver'
-            strokeWidth='2'
-            strokeDasharray='4,2'
-          />
-        </svg>
-      );
-    }
-  }
-
   render() {
     const objective = this.props.objective;
     if (!objective || this.props.users.isEmpty()) {
@@ -80,7 +40,7 @@ class OkrCard extends Component {
     }
     const user = this.props.users.find(user => user.get('ownerId') === objective.get('ownerId'));
     return (
-      <Card className={`okr-card ${this.props.isSelected ? 'active' : ''}`} ref='card'>
+      <Card className={`okr-card ${this.props.isSelected ? 'active' : ''}`}>
         <Card.Content>
           <Card.Header>
             <Avatar user={user} />
@@ -94,8 +54,6 @@ class OkrCard extends Component {
           <Icon link name='write' className='add-button' color='red' circular inverted
                 onClick={() => this.props.openOkrFormModal(objective.get('id'), { okrType: 'objective' })} />
         </Card.Content>
-        {this.pathSvg(true, !!objective.get('parentObjectiveId'))}
-        {this.pathSvg(false, !objective.get('childObjectives').isEmpty())}
       </Card>
     );
   }
