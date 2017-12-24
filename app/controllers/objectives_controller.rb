9462e3245276f4bfa27b51939eef104267128cd3
nishikata-seiichi <seiichi_nishikata@r.recruit.co.jp>
2017-12-25 03:11:43 +0900

    fix: /objectives APIの脆弱性対応

--- app/controllers/objectives_controller.rb
+++ app/controllers/objectives_controller.rb
@@ -1,37 +1,56 @@
 class ObjectivesController < ApplicationController
   def index
-    @user = User.find(params['user_id'])
-    @objectives = @user.objectives.where(okr_period_id: params['okr_period_id']).order(created_at: :desc)
+    @user = User.find(params[:user_id])
+    forbidden and return unless valid_permission?(@user.organization.id)
+
+    @objectives = @user.objectives.where(okr_period_id: params[:okr_period_id]).order(created_at: :desc)
   end
 
   def create
-    @objective = Objective.new(
-      params.require(:objective).permit(:name, :description, :owner_id, :parent_objective_id).merge(
-        okr_period_id: current_user.organization.latest_okr_period_id
-      )
+    create_params = objective_create_params.merge(
+      okr_period_id: current_organization.latest_okr_period_id
     )
+    return forbidden unless valid_permission?(Owner.find(create_params[:owner_id]).organization.id)
+
+    @objective = Objective.new(create_params)
     if @objective.save
       render status: :created
     else
-      render json: @objective.errors, status: :unprocessable_entity
+      unprocessable_entity_with_errors(@objective.errors)
     end
   end
 
   def update
     @objective = Objective.find(params[:id])
-    if @objective.update(params.require(:objective).permit(:name, :description, :progress_rate, :owner_id))
+    forbidden and return unless valid_permission?(@objective.owner.user.organization.id)
+
+    if @objective.update(objective_update_params)
       render action: :create, status: :ok
     else
-      render json: @objective.errors, status: :unprocessable_entity
+      unprocessable_entity_with_errors(@objective.errors)
     end
   end
 
   def destroy
     @objective = Objective.find(params[:id])
+    forbidden and return unless valid_permission?(@objective.owner.user.organization.id)
+
     if @objective.destroy
       head :no_content
     else
-      render json: @objective.errors, status: :unprocessable_entity
+      unprocessable_entity_with_errors(@objective.errors)
     end
+  end
+
+  private
+
+  def objective_create_params
+    params.require(:objective)
+      .permit(:name, :description, :owner_id, :parent_objective_id)
+  end
+
+  def objective_update_params
+    params.require(:objective)
+      .permit(:name, :description, :progress_rate, :owner_id)
   end
 end
