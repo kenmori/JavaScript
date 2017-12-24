14ea2ffc0031754f6b2a860b2412a29cdac4c98b
nishikata-seiichi <seiichi_nishikata@r.recruit.co.jp>
2017-12-25 03:46:57 +0900

    fix: /key_results APIの脆弱性対応

--- app/controllers/key_results_controller.rb
+++ app/controllers/key_results_controller.rb
@@ -1,12 +1,18 @@
 class KeyResultsController < ApplicationController
   def index
-    render json: KeyResult.all
+    render json: KeyResult
+                   .joins({ owner: { user: :organization_member } })
+                   .where(organization_members: { organization_id: current_organization.id })
   end
 
   def create
+    forbidden and return unless valid_permission?(Objective.find(params[:key_result][:objective_id]).owner.organization.id)
+    forbidden and return unless valid_permission?(Owner.find(params[:key_result][:owner_id]).organization.id)
+
     ActiveRecord::Base.transaction do
       @key_result = KeyResult.create!(key_result_create_params)
       params[:key_result][:key_result_members].each do |id|
+        # FIXME: 任意のユーザIDで作成してしまうが、サーバ側で採番しない？
         @key_result.key_result_members.create!(user_id: id)
       end
     end
@@ -16,23 +22,27 @@ class KeyResultsController < ApplicationController
   end
 
   def update
+    @key_result = KeyResult.find(params[:id])
+    forbidden and return unless valid_permission?(@key_result.objective.owner.organization.id)
+
     ActiveRecord::Base.transaction do
-      @key_result = KeyResult.find(params[:id])
       @key_result.update!(key_result_update_params)
       update_key_result_members if params[:key_result][:key_result_member]
       update_comment if params[:key_result][:comment]
     end
     render action: :create, status: :ok
   rescue
-    render json: @key_result.errors, status: :unprocessable_entity
+    unprocessable_entity_with_errors(@key_result.errors)
   end
 
   def destroy
     @key_result = KeyResult.find(params[:id])
+    forbidden and return unless valid_permission?(@key_result.objective.owner.organization.id)
+
     if @key_result.destroy
       render action: :create, status: :ok
     else
-      render json: @key_result.errors, status: :unprocessable_entity
+      unprocessable_entity_with_errors(@key_result.errors)
     end
   end
 
@@ -43,6 +53,7 @@ class KeyResultsController < ApplicationController
     if key_result_member_data['behavior'] == 'add'
       @key_result.key_result_members.create!(user_id: key_result_member_data['data'])
     elsif key_result_member_data['behavior'] == 'remove'
+      # FIXME: 任意のユーザIDで作成してしまうが、サーバ側で採番しない？
       person = @key_result.key_result_members.find_by(user_id: key_result_member_data['data'])
       person.destroy!
     end
@@ -61,8 +72,6 @@ class KeyResultsController < ApplicationController
       comment.destroy!
     end
   end
-
-  private
 
   def key_result_create_params
     params.require(:key_result)
