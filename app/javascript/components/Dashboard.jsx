4bcea8e769ba0970cb5534729eb93b6fbe21213e
rockwillj <rockwillj@gmail.com>
2017-12-30 13:11:21 +0900

    Fix: Objective のないユーザー選択時に OKR 一覧表示でエラーとなるバグ、同じ個数のユーザー選択時に最初の Objective  が選択されないバグを修正

--- app/javascript/components/Dashboard.jsx
+++ app/javascript/components/Dashboard.jsx
@@ -19,17 +19,20 @@ export default class Dashboard extends Component {
     const [nextOkrPeriodId, nextUserId] = [nextProps.menu.get('okrPeriodId'), nextProps.menu.get('userId')];
     if (okrPeriodId !== nextOkrPeriodId || userId !== nextUserId) {
       this.props.fetchObjectives(nextOkrPeriodId, nextUserId);
+    } else if (this.props.objectives !== nextProps.objectives) {
+      // Objective 一覧取得時や追加/削除時は最初の Objective を選択する
+      this.selectObjective(nextProps.objectives.first())
     }
   }
 
   selectObjective = objective => {
     this.setState({
-      selectedObjectiveId: objective.get('id'),
+      selectedObjectiveId: objective ? objective.get('id') : null,
     });
   }
 
   render() {
-    if(!this.props.objectives) return;
+    if (!this.props.objectives) return;
     const selectedObjective = this.props.objectives.find((objective) => objective.get('id') === this.state.selectedObjectiveId);
     return (
       <div className="dashboard">
