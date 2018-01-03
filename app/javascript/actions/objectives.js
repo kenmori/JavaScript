f026bce35c4c00d593c85d58633fd9fbb3235e99
shinot <shinot8324@gmail.com>
2018-01-03 17:51:48 +0900

    Refactor: normalizeする処理を共通のメソッドに切り出した。

--- app/javascript/actions/objectives.js
+++ app/javascript/actions/objectives.js
@@ -3,22 +3,19 @@ import actionTypes from '../constants/actionTypes';
 import { schema, normalize } from 'normalizr';
 import { fromJS } from 'immutable';
 import { objectiveListSchema } from '../schemas/index'
+
+function normalizeObjectives(objectives) {
+  const normalized = normalize(objectives, objectiveListSchema);
+  return fromJS(normalized)
+}
+
 const actions = createActions({
   [actionTypes.FETCH_OBJECTIVES]: (okrPeriodId, userId) => ({ okrPeriodId, userId }),
-  [actionTypes.FETCHED_OBJECTIVES]: (objectives) => {
-    const normalized = normalize(objectives.toJSON(), objectiveListSchema);
-    return fromJS(normalized)
-  },
+  [actionTypes.FETCHED_OBJECTIVES]: (objectives) => (normalizeObjectives(objectives.toJSON())),
   [actionTypes.ADD_OBJECTIVE]: (objective) => ({ objective }),
-  [actionTypes.ADDED_OBJECTIVE]: objective => {
-    const normalized = normalize([objective.toJSON()], objectiveListSchema);
-    return fromJS(normalized);
-  },
+  [actionTypes.ADDED_OBJECTIVE]: objective => (normalizeObjectives([objective.toJSON()])),
   [actionTypes.UPDATE_OBJECTIVE]: (objective) => ({ objective }),
-  [actionTypes.UPDATED_OBJECTIVE]: objective => {
-    const normalized = normalize([objective.toJSON()], objectiveListSchema);
-    return fromJS(normalized);
-  },
+  [actionTypes.UPDATED_OBJECTIVE]: objective => (normalizeObjectives([objective.toJSON()])),
   [actionTypes.REMOVE_OBJECTIVE]: (id) => ({ id }),
   [actionTypes.REMOVED_OBJECTIVE]: (id) => ({ id }),
 });
