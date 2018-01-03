dc093264d5486d15cd12f926ab1ab6175ed9e34e
rockwillj <rockwillj@gmail.com>
2018-01-03 23:21:55 +0900

    Fix: AWS 環境で画像がアップロードできなくなっていたデグレードを修正 (CarrierWave の host を指定すると S3 が利用できない)

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,8 +4,8 @@ CarrierWave.configure do |config|
     aws_access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID', 'access_key_id'),
     aws_secret_access_key: ENV.fetch('AWS_SECRET_KEY', 'secret_key'),
     region: ENV.fetch('AWS_REGION', 'ap-northeast-1'),
-    host: ENV.fetch('AWS_S3_HOST', 's3.amazonaws.com'),
-    endpoint: ENV.fetch('AWS_S3_ENDPOINT', nil),
+    host: ENV.fetch('MINIO_HOST', nil),
+    endpoint: ENV.fetch('MINIO_ENDPOINT', nil),
     path_style: true
   }
   config.fog_provider = 'fog/aws'
