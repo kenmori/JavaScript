015f24b8e9563c48867def1c21b13459623bc261
shinot <shinot8324@gmail.com>
2018-01-06 16:20:55 +0900

    Fix: OKR期間の表示名用のカラムnameを追加した。不要となったカラムを削除した。

--- db/migrate/20170806114431_create_okr_periods.rb
+++ db/migrate/20170806114431_create_okr_periods.rb
@@ -3,12 +3,10 @@ class CreateOkrPeriods < ActiveRecord::Migration[5.1]
     create_table :okr_periods do |t|
       t.timestamps
       t.integer :organization_id, null: false
-      t.integer :year,            null: false             # 年度
-      t.integer :period_number,   null: false             # 第1期などの表現に使用する数字。4半期であれば1~4, 半期であれば1~2
-      t.integer :status,          null: false, default: 0 # ステータス(アクティブ: 1, インアクティブ: 0) 現在のperiodであればアクティブ
       t.index   :organization_id
       t.date    :month_start,     null: false
       t.date    :month_end,       null: false
+      t.string  :name
     end
   end
 end
