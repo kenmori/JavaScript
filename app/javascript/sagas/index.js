3f7c6c8e126ee4db9e5d1ff17acb88bebab3dcca
rockwillj <rockwillj@gmail.com>
2018-01-09 14:15:25 +0900

    Remove: 未使用の OKR 設定 (モデル、API、設定画面など) を削除 (唯一使われてた OKR 期間は Organization に持たせる)

--- app/javascript/sagas/index.js
+++ app/javascript/sagas/index.js
@@ -2,7 +2,6 @@ import { fork } from 'redux-saga/effects';
 import { objectiveSagas } from './objectives';
 import { keyResultSagas } from './keyResults';
 import { userSagas } from './users';
-import { okrSettingSagas } from './okrSettings';
 import { sessionSagas } from './sessions';
 import { organizationSagas } from './organizations';
 
@@ -10,7 +9,6 @@ const sagas = [
   objectiveSagas,
   keyResultSagas,
   userSagas,
-  okrSettingSagas,
   sessionSagas,
   organizationSagas,
 ];
--- db/migrate/20170802134121_create_organizations.rb
+++ db/migrate/20170802134121_create_organizations.rb
@@ -5,6 +5,7 @@ class CreateOrganizations < ActiveRecord::Migration[5.1]
       t.string :name, null: false   # 組織名
       t.string :uniq_name, null: false
       t.string :logo
+      t.integer :okr_span, null: false, default: 3  # OKR 期間
     end
   end
 end
