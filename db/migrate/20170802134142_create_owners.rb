d1a1b7f5b087b775a3677b0be64de2790290e105
rockwillj <rockwillj@gmail.com>
2018-01-09 18:43:01 +0900

    Remove: 不要になった Owner モデルを削除

--- db/migrate/20170802134142_create_owners.rb
+++
@@ -1,8 +0,0 @@
-class CreateOwners < ActiveRecord::Migration[5.1]
-  def change
-    create_table :owners do |t|
-      t.timestamps
-      t.integer :kind, null: false # 所有者の種別(グループORユーザー)
-    end
-  end
-end
