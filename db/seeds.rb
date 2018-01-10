3119aed4ffec8839a05ec50b5f426c29eeae8d1f
rockwillj <rockwillj@gmail.com>
2018-01-11 01:28:55 +0900

    Modify: schema と seeds を *Member モデルに対応させる

--- db/seeds.rb
+++ db/seeds.rb
@@ -65,48 +65,48 @@ ApplicationRecord.transaction do
       name: '3Q'
   )
 
-  active_objective1 = login_user.owner.objectives.create!(
+  active_objective1 = login_user.objectives.create!(
     name: '使いやすいサービスを作る',
     description: '事業を成功させるには、少なくとも競合より使いやすいサービスが欲しい。',
     okr_period_id: active_okr_period.id,
   )
-  active_key_result1 = active_objective1.key_results.create!(
+  active_key_result1 = login_user.key_results.create!(
       name: 'イケてるエンジニアを採用する',
       okr_period_id: active_okr_period.id,
-      owner_id: login_user.owner_id,
+      objective_id: active_objective1.id,
       target_value: 1,
       actual_value: 0,
       value_unit: '人',
       expired_date: '2017-11-30'
   )
-  active_key_result2 = active_objective1.key_results.create!(
+  active_key_result2 = another.key_results.create!(
       name: '正式版をリリースする',
       okr_period_id: active_okr_period.id,
-      owner_id: another.owner_id,
+      objective_id: active_objective1.id,
       expired_date: '2017-11-30'
   )
 
-  active_objective2 = active_objective1.child_objectives.create!(
+  active_objective2 = login_user.objectives.create!(
     name: 'シニアエンジニアを採用する',
     description: '開発を加速するには、シニアエンジニア級のスキルを持ったエンジニアが必要。',
     okr_period_id: active_okr_period.id,
-    owner_id: login_user.owner_id,
+    parent_objective_id: active_objective1.id,
     parent_key_result_id: active_key_result1.id,
   )
-  active_objective2.key_results.create!(
+  login_user.key_results.create!(
       name: '5人を面接する',
       okr_period_id: active_okr_period.id,
-      owner_id: login_user.owner_id,
+      objective_id: active_objective2.id,
       target_value: 5,
       actual_value: 3,
       value_unit: '人',
       progress_rate: 60,
       expired_date: '2017-11-30'
   )
-  active_objective2.key_results.create!(
+  another.key_results.create!(
       name: '採用サービスを2つ利用する',
       okr_period_id: active_okr_period.id,
-      owner_id: another.owner_id,
+      objective_id: active_objective2.id,
       target_value: 2,
       actual_value: 2,
       value_unit: 'サービス',
@@ -114,24 +114,24 @@ ApplicationRecord.transaction do
       expired_date: '2017-10-31'
   )
 
-  active_objective3 = active_objective1.child_objectives.create!(
+  active_objective3 = another.objectives.create!(
     name: 'MVP の機能を開発する',
     description: '方針として MVP の機能にフォーカスすることで、確実に正式版のリリース日までに実装を完了する。',
     okr_period_id: active_okr_period.id,
-    owner_id: another.owner_id,
+    parent_objective_id: active_objective1.id,
     parent_key_result_id: active_key_result2.id,
   )
-  active_objective3.key_results.create!(
+  another.key_results.create!(
       name: 'ログイン機能を実装する',
       okr_period_id: active_okr_period.id,
-      owner_id: another.owner_id,
+      objective_id: active_objective3.id,
       progress_rate: 70,
       expired_date: '2017-10-31'
   )
-  active_objective3.key_results.create!(
+  another.key_results.create!(
       name: 'ユーザー登録機能を実装する',
       okr_period_id: active_okr_period.id,
-      owner_id: another.owner_id,
+      objective_id: active_objective3.id,
       progress_rate: 30,
       expired_date: '2017-11-30'
   )
@@ -142,135 +142,117 @@ ApplicationRecord.transaction do
       month_end: '2017/11/30',
   )
 
-  inactive_objective1 = login_user.owner.objectives.create!(
+  inactive_objective1 = login_user.objectives.create!(
       name: 'プロトタイプを作る',
       description: 'ビジネスモデルを仮説検証するために実際に動かすことのできるプロトタイプが必須。',
       okr_period_id: inactive_okr_period.id,
   )
-  inactive_key_result1 = inactive_objective1.key_results.create!(
+  inactive_key_result1 = login_user.key_results.create!(
       name: 'アプリの仕様をおおまかに決める',
       okr_period_id: inactive_okr_period.id,
-      owner_id: login_user.owner_id,
+      objective_id: inactive_objective1.id,
       expired_date: '2017-09-30'
   )
-  inactive_key_result2 = inactive_objective1.key_results.create!(
+  inactive_key_result2 = another.key_results.create!(
       name: 'プロトアプリを動かせるようにする',
       okr_period_id: inactive_okr_period.id,
-      owner_id: another.owner_id,
+      objective_id: inactive_objective1.id,
       expired_date: '2017-09-30'
   )
 
-  inactive_objective2 = inactive_objective1.child_objectives.create!(
+  inactive_objective2 = login_user.objectives.create!(
       name: 'アプリの外部仕様を策定する',
       description: 'プロトタイプを開発するために画面や機能に関する外部仕様が求められる。',
       okr_period_id: inactive_okr_period.id,
-      owner_id: login_user.owner_id,
+      parent_objective_id: inactive_objective1.id,
       parent_key_result_id: inactive_key_result1.id,
   )
-  inactive_objective2.key_results.create!(
+  login_user.key_results.create!(
       name: 'ペルソナを絞り込む',
       okr_period_id: inactive_okr_period.id,
-      owner_id: login_user.owner_id,
+      objective_id: inactive_objective2.id,
       target_value: 3,
       actual_value: 3,
       value_unit: '人',
       progress_rate: 100,
       expired_date: '2017-07-31'
   )
-  inactive_objective2.key_results.create!(
+  login_user.key_results.create!(
       name: 'ユーザーストーリーを考える',
       okr_period_id: inactive_okr_period.id,
-      owner_id: login_user.owner_id,
+      objective_id: inactive_objective2.id,
       target_value: 5,
       actual_value: 3,
       value_unit: '個',
       progress_rate: 60,
       expired_date: '2017-07-31'
   )
-  inactive_objective2.key_results.create!(
+  login_user.key_results.create!(
       name: 'ビジネス要件をまとめる',
       okr_period_id: inactive_okr_period.id,
-      owner_id: login_user.owner_id,
+      objective_id: inactive_objective2.id,
       progress_rate: 60,
       expired_date: '2017-08-31'
   )
-  inactive_objective2.key_results.create!(
+  another.key_results.create!(
       name: '外部仕様を策定する',
       okr_period_id: inactive_okr_period.id,
-      owner_id: another.owner_id,
+      objective_id: inactive_objective2.id,
       progress_rate: 40,
       expired_date: '2017-09-30'
   )
 
-  inactive_objective3 = inactive_objective1.child_objectives.create!(
+  inactive_objective3 = another.objectives.create!(
       name: 'プロトアプリをサーバーにデプロイする',
       description: '開発環境以外でもアプリを触れるようにするためサーバー上でアプリを動かす。',
       okr_period_id: inactive_okr_period.id,
-      owner_id: another.owner_id,
+      parent_objective_id: inactive_objective1.id,
       parent_key_result_id: inactive_key_result2.id,
   )
-  inactive_objective3.key_results.create!(
+  login_user.key_results.create!(
       name: 'ワイヤーフレームを作る',
       okr_period_id: inactive_okr_period.id,
-      owner_id: login_user.owner_id,
+      objective_id: inactive_objective3.id,
       progress_rate: 80,
       expired_date: '2017-07-31'
   )
-  inactive_objective3.key_results.create!(
+  another.key_results.create!(
       name: 'モックアップを作る',
       okr_period_id: inactive_okr_period.id,
-      owner_id: another.owner_id,
+      objective_id: inactive_objective3.id,
       progress_rate: 90,
       expired_date: '2017-08-31'
   )
-  inactive_objective3.key_results.create!(
+  another.key_results.create!(
       name: 'プロトタイプを実装する',
       okr_period_id: inactive_okr_period.id,
-      owner_id: another.owner_id,
+      objective_id: inactive_objective3.id,
       progress_rate: 70,
       expired_date: '2017-09-30'
   )
-  inactive_objective3.key_results.create!(
+  another.key_results.create!(
       name: 'サーバー環境を構築する',
       okr_period_id: inactive_okr_period.id,
-      owner_id: another.owner_id,
+      objective_id: inactive_objective3.id,
       progress_rate: 100,
       expired_date: '2017-09-30'
   )
 
 # 開発部の今期のOKRを作成
   development_group = organization.groups.create(name: '開発部')
-  development_group.members.create(user_id: login_user.id)
-  development_group_objective = development_group.owner.objectives.create(
-    name: 'ビジネス成長に寄与する機能を開発する',
-    description: '要検討',
-    okr_period_id: active_okr_period.id,
-  )
-  development_group_objective.child_objectives.create!(
-    name: '×××をリリース',
+  development_group.group_members.create(user_id: another.id)
+  development_group.owner.objectives.create(
+    name: '生産的な開発体制を構築する',
     description: '',
-    okr_period_id: active_okr_period.id,
-    owner_id: development_group.owner_id
+    okr_period_id: inactive_okr_period.id,
   )
-  # development_group_objective.child_objectives.create!(
-  #     name: '◯◯◯リリース',
-  #     description: '',
-  #     okr_period_id: active_okr_period.id,
-  #     owner_id: login_user.owner_id
-  # )
 
 # マーケティング部の今期のOKRを作成
   marketing_group = organization.groups.create(name: 'マーケティング部')
-  marketing_group.members.create(user_id: login_user.id)
-  marketing_group_objective = marketing_group.owner.objectives.create(
-    name: 'ビジネス成長に寄与するマーケティングを実施する',
-    description: '要検討',
-    okr_period_id: active_okr_period.id
+  marketing_group.group_members.create(user_id: login_user.id)
+  marketing_group.owner.objectives.create(
+    name: 'グローバルマーケティングを実施する',
+    description: '',
+    okr_period_id: inactive_okr_period.id
   )
-  # marketing_group_objective.child_objectives.create(
-  #   name: '×××を実施する',
-  #   description: '',
-  #   okr_period_id: active_okr_period.id,
-  #   owner_id: marketing_group.id
-  # )
 end
