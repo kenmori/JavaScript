2d385d0b8a54d5d058c8af1826df284822dd537a
ikumi.toshihiko <ikumit.org@gmail.com>
2018-01-14 20:40:30 +0900

    エラーモーダル時にローディングが表示されるバグ修正

--- app/javascript/utils/withLoading.js
+++ app/javascript/utils/withLoading.js
@@ -1,4 +1,4 @@
-import { put, call, fork } from 'redux-saga/effects';
+import { put, call, fork, select } from 'redux-saga/effects';
 import loadingActions from '../actions/loading';
 
 const TIME_TO_OPEN_LOADING = 200;
@@ -16,17 +16,23 @@ function wait(time) {
 
 function* openLoading() {
   yield call(wait(TIME_TO_OPEN_LOADING));
-  if(connectingCounter) {
+  const state = yield select();
+  if(!state.loading.get('isForceCloseLoading') && connectingCounter) {
     yield put(loadingActions.openLoading());  
   }
 }
 
 export default function withLoading(xhrRequest){
   return function* (){
+    const state = yield select();
+    if(state.loading.get('isForceCloseLoading')) {
+      yield put(loadingActions.forceCloseLoadingOff());
+    }
+    
     if(++connectingCounter === 1) {
       yield fork(openLoading);  
     }
-    yield xhrRequest(...arguments);
+    const response = yield xhrRequest(...arguments);
     if(--connectingCounter === 0) {
       yield put(loadingActions.closeLoading());
     }
