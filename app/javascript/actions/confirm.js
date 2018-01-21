892dc200e6b456f777113788b028e14597fb434c
ikumi.toshihiko <ikumit.org@gmail.com>
2018-01-21 13:41:48 +0900

    リファクタ

--- app/javascript/actions/confirm.js
+++ app/javascript/actions/confirm.js
@@ -3,7 +3,6 @@ import actionTypes from '../constants/actionTypes';
 
 const actions = createActions({
   [actionTypes.OPEN_CONFIRM]: (confirmParams) => {
-    console.log(44, confirmParams)
     return { confirmParams };
   },
   [actionTypes.CLOSE_CONFIRM]: () => {},
