017f0470936248c2a31c3a97c7881daf988ba6ab
rockwillj <rockwillj@gmail.com>
2018-02-16 21:06:52 +0900

    Refactor: redux-logger: timestamp の代わりに duration を表示する

--- app/javascript/stores/index.js
+++ app/javascript/stores/index.js
@@ -9,7 +9,10 @@ export default function configureStore() {
   const middlewares = [sagaMiddleware];
   if (process.env.NODE_ENV !== 'production') {
     const { createLogger } = require(`redux-logger`);
-    const logger = createLogger();
+    const logger = createLogger({
+      duration: true,
+      timestamp: false,
+    });
     middlewares.push(logger);
   }
 
