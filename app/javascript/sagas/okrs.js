ae0f21151a99cf036e4c312317b6444a2e4e47a1
rockwillj <rockwillj@gmail.com>
2018-02-22 16:40:27 +0900

    Fix: ローディングがユーザー切替時に表示されなかったり、逆に全 KR 取得完了まで消えなかったりしていた (fetchOkrs() 呼び出し中のローディング表示制御が不適切だった)

--- app/javascript/sagas/okrs.js
+++ app/javascript/sagas/okrs.js
@@ -1,30 +1,20 @@
 import { all, put, takeLatest } from 'redux-saga/effects';
-import call from '../utils/call';
-import API from '../utils/api';
 import objectiveActions from '../actions/objectives';
 import keyResultActions from '../actions/keyResults';
-import loadingActions from '../actions/loading';
 import actionTypes from '../constants/actionTypes';
-import withLoading from '../utils/withLoading';
 
 function* fetchOkrs({ payload }) {
-  let result = yield call(API.get, '/objectives', { okrPeriodId: payload.okrPeriodId, userId: payload.userId });
-  yield put(objectiveActions.fetchedObjectives(result.get('objectives')));
+  yield put(objectiveActions.fetchObjectives(payload.okrPeriodId, payload.userId));
 
-  yield put(loadingActions.forceCloseLoadingOff());
-  yield put(loadingActions.closeLoading());
-
-  result = yield call(API.get, '/key_results', { okrPeriodId: payload.okrPeriodId, userId: payload.userId });
-  yield put(keyResultActions.fetchedKeyResults(result.get('keyResults')));
+  yield put(keyResultActions.fetchKeyResults(payload.okrPeriodId, payload.userId));
 
   if (payload.withAllKeyResults) {
-    result = yield call(API.get, '/key_results', { okrPeriodId: payload.okrPeriodId });
-    yield put(keyResultActions.fetchedAllKeyResults(result.get('keyResults')));
+    yield put(keyResultActions.fetchAllKeyResults(payload.okrPeriodId));
   }
 }
 
 export function* okrSagas() {
   yield all([
-    takeLatest(actionTypes.FETCH_OKRS, withLoading(fetchOkrs)),
+    takeLatest(actionTypes.FETCH_OKRS, fetchOkrs),
   ]);
 }
