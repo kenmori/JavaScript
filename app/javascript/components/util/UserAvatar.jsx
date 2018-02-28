619c960d0074d6e1674d01aea59b31ea67383c19
rockwillj <rockwillj@gmail.com>
2018-02-28 18:34:48 +0900

    Modify: UserAvatar で画像変更可の場合はホバー時にアバター右下に鉛筆アイコンを表示する

--- app/javascript/components/util/UserAvatar.jsx
+++ app/javascript/components/util/UserAvatar.jsx
@@ -1,7 +1,17 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Popup } from 'semantic-ui-react';
+import { Popup, Icon } from 'semantic-ui-react';
 import Avatar from './Avatar';
+
+const sizeToIconSize = {
+  mini: 'small',
+  tiny: 'small',
+  small: null,
+  large: 'large',
+  big: 'large',
+  huge: 'big',
+  massive: 'huge',
+};
 
 class UserAvatar extends Component {
 
@@ -15,19 +25,26 @@ class UserAvatar extends Component {
     this.props.openAvatarModal(this.props.user.get('id'), event.target.files[0]);
     event.target.value = null;
   }
-  
+
+  trigger = () => {
+    return (
+      <div className={`user_avatar ${this.props.readOnly ? 'readonly' : 'changeable'}`} onClick={this.clickFileInput}>
+        <Avatar {...this.props} />
+        <input type='file' ref='fileInput' onChange={this.openAvatarModal} />
+        {!this.props.readOnly && <Icon name='pencil' size={sizeToIconSize[this.props.size]} color='grey' />}
+      </div>
+    );
+  }
+
   popupContent = () => {
     return `${this.props.user.get('lastName')} ${this.props.user.get('firstName')}`;
   }
 
   render() {
     return (
-      <div className={`user_avatar ${this.props.readOnly ? 'readonly' : 'changeable'}`} onClick={this.clickFileInput}>
-        <Popup size='tiny' trigger={<div><Avatar {...this.props} /></div>}>
-          <Popup.Content>{this.popupContent()}</Popup.Content>
-        </Popup>
-        <input type='file' ref='fileInput' onChange={this.openAvatarModal} />
-      </div>
+      <Popup size='tiny' trigger={this.trigger()}>
+        <Popup.Content>{this.popupContent()}</Popup.Content>
+      </Popup>
     );
   }
 }
