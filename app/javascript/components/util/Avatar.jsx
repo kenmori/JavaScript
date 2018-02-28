bc05e1a7ffc964b50d6f1e4b3b6d2d0d64345b58
rockwillj <rockwillj@gmail.com>
2018-02-28 20:23:49 +0900

    Modify: 無効なユーザーの場合はアバターの表示スタイルを変更する (透過表示、禁止アイコン表示、(無効) テキスト表示)

--- app/javascript/components/util/Avatar.jsx
+++ app/javascript/components/util/Avatar.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import UserAvatar from 'react-user-avatar';
+import { Icon } from 'semantic-ui-react';
 import avatar_image from '../../images/avatar.png';
 
 const sizeToNum = {
@@ -13,12 +14,23 @@ const sizeToNum = {
   massive: 128,
 };
 
+const sizeToIconSize = {
+  mini: 'small',
+  tiny: 'small',
+  small: null,
+  large: 'large',
+  big: 'large',
+  huge: 'big',
+  massive: 'huge',
+};
+
 class Avatar extends Component {
   render() {
     let name = this.props.user.get('lastName');
     let path = this.props.user.get('avatarUrl');
     const size = this.props.size;
-    const fullName = `${this.props.user.get('lastName')} ${this.props.user.get('firstName')}`;
+    let fullName = `${this.props.user.get('lastName')} ${this.props.user.get('firstName')}`;
+    const disabled = this.props.user.get('disabled');
 
     if (size === 'tiny' || size === 'mini') {
       name = [...name][0]; // 先頭1文字のみを表示する (サロゲートペア考慮済み)
@@ -26,15 +38,19 @@ class Avatar extends Component {
     if (!this.props.withInitial) {
       path = path || avatar_image; // イニシャルを非表示にするためデフォルト画像を指定する
     }
+    if (disabled) {
+      fullName = `${fullName} (無効)`
+    }
 
     return (
-      <div className='avatar'>
+      <div className={`avatar ${disabled ? 'disabled' : ''}`}>
         <UserAvatar className={`avatar__inner is-${size}`}
                     src={path}
                     name={name}
                     size={sizeToNum[size]}
                     color='transparent'
         />
+        {disabled && <Icon disabled name='dont' size={sizeToIconSize[this.props.size]} />}
         {this.props.withName && <span className='avatar__name'>{fullName}</span>}
       </div>
     );
