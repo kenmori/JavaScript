11382cb032a00a86852c4ae794a771bd71213351
ikumi.toshihiko <ikumit.org@gmail.com>
2018-02-28 21:50:47 +0900

    Sidebar: Keyresultがcurrent状態にならないバグシュウセイ

--- app/javascript/containers/Home.jsx
+++ app/javascript/containers/Home.jsx
@@ -1,6 +1,7 @@
 import Home from '../components/Home';
 import { connect } from 'react-redux';
 import { hashids, OKR_TYPE_ID } from '../utils/hashids';
+import history from '../utils/history';
 import objectiveActions from '../actions/objectives';
 import dialogActions from '../actions/dialogs';
 import currentActions from '../actions/current';
@@ -11,12 +12,17 @@ const mapStateToProps = (state, { match: { params } }) => {
   const fetchedObjectiveId = state.objectives.get('fetchedObjective');
   const allKeyResults = denormalizeKeyResults(state.keyResults.get('allIds'), state.entities);
   const [okrTypeId, okrId] = hashids.decode(params.okrHash);
-  let objectiveId = okrTypeId === OKR_TYPE_ID.OBJECTIVE ? okrId : 0;
+  let objectiveId = okrTypeId === OKR_TYPE_ID.OBJECTIVE ? okrId : null;
   const keyResultId = okrTypeId === OKR_TYPE_ID.KEY_RESULT ? okrId : null;
-  const okrType = objectiveId && keyResultId ? "keyResult" : objectiveId ? "objective" : "";
+  const okrType = okrTypeId === OKR_TYPE_ID.OBJECTIVE ? 'objective' : okrTypeId === OKR_TYPE_ID.KEY_RESULT ? 'keyResult' : null;
   
   if (!allKeyResults.isEmpty() && !!keyResultId) {
-    objectiveId = allKeyResults.find(item => item.get('id') === keyResultId).get('objectiveId');
+    const targetKeyResults = allKeyResults.find(item => item.get('id') === keyResultId);
+    if(targetKeyResults) {
+      objectiveId = targetKeyResults.get('objectiveId')
+    } else {
+      history.push('/');
+    }
   }
 
   return {
