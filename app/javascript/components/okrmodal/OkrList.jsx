3678d429c571775239217e9f8c6204235fa85525
rockwillj <rockwillj@gmail.com>
2018-03-21 15:54:08 +0900

    Modify: OKR 編集モーダルの下位 Objective 一覧にリンクを設定＆進捗率を表示

--- app/javascript/components/okrmodal/OkrList.jsx
+++ app/javascript/components/okrmodal/OkrList.jsx
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { openObjective, openKeyResult } from '../../utils/linker';
 import { List } from 'semantic-ui-react';
 import OwnerAvatar from '../util/OwnerAvatar';
 
 class OkrList extends Component {
+
+  handleClick = id => {
+    this.props.isObjective ? openObjective(id) : openKeyResult(id);
+  }
 
   render() {
     return (
@@ -11,7 +16,10 @@ class OkrList extends Component {
         {this.props.okrs.map(okr =>
           <List.Item key={okr.get('id')}>
             <OwnerAvatar owner={okr.get('owner')} />
-            <List.Content>{okr.get('name')}</List.Content>
+            <List.Content>
+              <a onClick={() => this.handleClick(okr.get('id'))}>{okr.get('name')}</a>
+            </List.Content>
+            <List.Content>{okr.get('progressRate')}%</List.Content>
           </List.Item>
         )}
       </List>
@@ -21,6 +29,11 @@ class OkrList extends Component {
 
 OkrList.propTypes = {
   okrs: PropTypes.object.isRequired,
+  isObjective: PropTypes.bool,
+};
+
+OkrList.defaultProps = {
+  isObjective: true,
 };
 
 export default OkrList;
