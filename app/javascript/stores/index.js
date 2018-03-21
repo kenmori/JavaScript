3206cc3d211f015b41bc5c84da1c4cc03b7ee58c
neko-neko <seiichi.nishikata456@gmail.com>
2018-03-21 20:55:12 +0900

    Add react-ga

--- app/javascript/stores/index.js
+++ app/javascript/stores/index.js
@@ -2,11 +2,12 @@ import { createStore, applyMiddleware } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 import reducers from '../reducers/index';
 import rootSaga from '../sagas/index';
+import { GA } from './ReactGAMiddleware';
 
 export default function configureStore() {
   const sagaMiddleware = createSagaMiddleware();
 
-  const middlewares = [sagaMiddleware];
+  const middlewares = [sagaMiddleware, GA];
   if (process.env.NODE_ENV !== 'production') {
     const { createLogger } = require(`redux-logger`);
     const logger = createLogger({
