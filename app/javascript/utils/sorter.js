2eb2982844be248a519a484ccc531b00a4544640
ikumi.toshihiko <ikumit.org@gmail.com>
2018-03-25 22:50:33 +0900

    Objectiveソート方法をKeyResultと同じ方式にリファクタ

--- app/javascript/utils/sorter.js
+++ app/javascript/utils/sorter.js
@@ -1,3 +1,5 @@
+export const sortObjectives = (objectives, order) => getSortedObjectives('id', objectives, order);
+
 export const sortChildKeyResults = (objective, order = null) => {
   const keyResults = objective.get('keyResults');
   order = Array.isArray(order) ? order : order ? JSON.parse(order) : null;
@@ -7,12 +9,14 @@ export const sortChildKeyResults = (objective, order = null) => {
   })
 }
 
-export const sortChildObjectives = (objectives, order) => {
-  order = order || "[]";
-  return objectives.sortBy((o) => JSON.parse(order).indexOf(o.get('parentKeyResultId')));
-}
+export const sortChildObjectives = (objectives, order) => getSortedObjectives('parentKeyResultId', objectives, order);
 
 export const createOrderData = (list) => JSON.stringify(list.map(c => c.get('id')).toArray());
+
+const getSortedObjectives = (key, objectives, order) => {
+  order = order || "[]";
+  return objectives.sortBy((o) => JSON.parse(order).indexOf(o.get(key)));
+}
 
 const getSortedKeyResults = (keyResults, order) => {
   return keyResults.sortBy((o) => order.indexOf(o.get('id')));
