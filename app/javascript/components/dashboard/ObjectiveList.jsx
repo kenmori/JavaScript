2487ba11f6625682f48aa20517a1d313514bdb7d
rockwillj <rockwillj@gmail.com>
2018-04-02 16:28:49 +0900

    Remove: ObjectiveList は DropTarget にする必要がないため該当するコードを削除

--- app/javascript/components/dashboard/ObjectiveList.jsx
+++ app/javascript/components/dashboard/ObjectiveList.jsx
@@ -1,15 +1,9 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { default as TouchBackend } from 'react-dnd-touch-backend';
-import { DropTarget, DragDropContext } from 'react-dnd'
+import { DragDropContext } from 'react-dnd'
 import HTML5Backend from 'react-dnd-html5-backend'
 import Objective from './Objective';
-
-function collect(connect) {
-  return {
-    connectDropTarget: connect.dropTarget(),
-  };
-}
 
 class ObjectiveList extends Component {
   constructor() {
@@ -33,26 +27,6 @@ class ObjectiveList extends Component {
 			index: objectives.indexOf(objective),
 		}
   }
-  
-  objectiveListHTML = () => (
-    <div className={`objective-list ${this.state.isDragging ? 'is-dragging' : ''}`}>
-      {
-        this.props.objectives.map((objective) => {
-          const isSelected = objective.get('id') === this.props.currentObjectiveId;
-          return <Objective
-                  key={objective.get('id')}
-                  objective={objective}
-                  isSelected={isSelected}
-                  moveCard={this.props.replaceObjectives}
-                  objectivesLength={this.props.objectives.size}
-                  updateUserObjectiveOrder={this.props.updateUserObjectiveOrder}
-                  findCard={this.findCard.bind(this)}
-                  changeDragStyle={this.changeDragStyle.bind(this)}
-                  isSelectedLoginUser={this.props.isSelectedLoginUser}
-                  selectObjective={this.selectObjective.bind(this)} />
-        })}
-    </div>
-  )
 
   selectObjective = objective => {
     this.props.setMapObjective(objective);
@@ -60,7 +34,24 @@ class ObjectiveList extends Component {
   }
 
   render() {
-    return this.props.connectDropTarget(this.objectiveListHTML());
+    return (
+      <div className={`objective-list ${this.state.isDragging ? 'is-dragging' : ''}`}>
+        {this.props.objectives.map((objective) => {
+          const isSelected = objective.get('id') === this.props.currentObjectiveId;
+          return <Objective
+            key={objective.get('id')}
+            objective={objective}
+            isSelected={isSelected}
+            moveCard={this.props.replaceObjectives}
+            objectivesLength={this.props.objectives.size}
+            updateUserObjectiveOrder={this.props.updateUserObjectiveOrder}
+            findCard={this.findCard.bind(this)}
+            changeDragStyle={this.changeDragStyle.bind(this)}
+            isSelectedLoginUser={this.props.isSelectedLoginUser}
+            selectObjective={this.selectObjective.bind(this)} />
+        })}
+      </div>
+    );
   }
 }
 
@@ -68,10 +59,9 @@ ObjectiveList.propTypes = {
   objectives: PropTypes.object.isRequired,
   isSelectedLoginUser: PropTypes.bool.isRequired,
   setMapObjective: PropTypes.func.isRequired,
-  connectDropTarget: PropTypes.func.isRequired,
 };
 
 const isTouchSupport = 'ontouchstart' in window;
 const backend = isTouchSupport ? TouchBackend : HTML5Backend;
 
-export default DragDropContext(backend)(DropTarget('card', {}, collect)(ObjectiveList));
+export default DragDropContext(backend)(ObjectiveList);
