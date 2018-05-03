5e4c8456b4da494933f1e5b613b2d30dd7e1a03a
rockwillj <rockwillj@gmail.com>
2018-05-03 22:33:20 +0900

    Refactor: KR 作成モーダル：フォーム部分をコンポーネントに切り出し

---
+++ app/javascript/components/keyresultmodal/KeyResultForm.jsx
@@ -0,0 +1,133 @@
+import React, { PureComponent } from 'react'
+import PropTypes from 'prop-types'
+import { Field } from 'redux-form'
+import { List } from 'immutable'
+import { Form, TextArea } from 'semantic-ui-react'
+import KeyResultMemberSelect from '../form/KeyResultMemberSelect'
+import UserSelect from '../form/UserSelect'
+import OkrSelect from '../form/OkrSelect'
+import RequiredLabel from '../form/RequiredLabel'
+import RenderField from '../form/RenderField'
+import RenderDateField from '../form/RenderDateField'
+import {
+  validateKeyResultName, validateTargetValue, validateExpiredDate, normalizeExpiredDate
+} from '../../utils/validator'
+
+class KeyResultForm extends PureComponent {
+
+  render() {
+    if (!this.props.objective) return null
+    return (
+      <div className="keyresult-modal__main">
+        <Form>
+          <Form.Group widths='equal'>
+            <Form.Field>
+              <RequiredLabel text='Key Result' />
+              <Field
+                name='name'
+                type='text'
+                component={RenderField}
+                validate={[validateKeyResultName]}
+              />
+            </Form.Field>
+          </Form.Group>
+          <Form.Group widths='equal'>
+            <Form.Field>
+              <label>説明</label>
+              <TextArea
+                autoHeight
+                rows={3}
+                onChange={(e, { value }) => this.props.onChange({ description: value })}
+                placeholder={`Key Result についての説明や補足を入力してください。\n説明を入力すると、メンバーに目指すべき方向性が伝わりやすくなります。`}
+              />
+            </Form.Field>
+          </Form.Group>
+          <Form.Group>
+            <Form.Field>
+              <div className="flex-start">
+                <div style={{ marginRight: "10px" }}>
+                  <RequiredLabel text='目標値' required={this.props.isRequiredTargetValue} />
+                  <div style={{ width: "177px" }}>
+                    <Field
+                      name='targetValue'
+                      type='text'
+                      component={RenderField}
+                      validate={[validateTargetValue]}
+                    />
+                  </div>
+                </div>
+                <div>
+                  <label>単位</label>
+                  <Field
+                    name='valueUnit'
+                    type='text'
+                    placeholder='例：円、件、人'
+                    component={RenderField}
+                    onChange={(e, newValue) => this.props.onChange({ isRequiredTargetValue: !!newValue })}
+                  />
+                </div>
+              </div>
+            </Form.Field>
+          </Form.Group>
+          <Form.Group>
+            <Form.Field>
+              <RequiredLabel text='期限' />
+              <Field
+                name='expiredDate'
+                component={RenderDateField}
+                validate={[validateExpiredDate]}
+                normalize={normalizeExpiredDate}
+              />
+            </Form.Field>
+          </Form.Group>
+          <Form.Group>
+            <Form.Field>
+              <RequiredLabel text='責任者' />
+              <UserSelect
+                users={this.props.users}
+                value={this.props.ownerId}
+                onChange={this.props.changeKeyResultOwner}
+              />
+            </Form.Field>
+          </Form.Group>
+          <Form.Group>
+            <Form.Field>
+              <label>関係者</label>
+              <KeyResultMemberSelect
+                users={this.props.users}
+                members={this.props.members}
+                excludedId={this.props.ownerId}
+                add={this.props.addMember}
+                remove={this.props.removeMember}
+              />
+            </Form.Field>
+          </Form.Group>
+          <Form.Field>
+            <RequiredLabel text='紐付く Objective' />
+            <OkrSelect
+              okrs={List.of(this.props.objective)}
+              value={this.props.objective.get('id')}
+              preview={false}
+              disabled={true}
+              onChange={value => {}}
+            />
+          </Form.Field>
+        </Form>
+      </div>
+    )
+  }
+}
+
+KeyResultForm.propTypes = {
+  objective: PropTypes.object,
+  users: PropTypes.object.isRequired,
+  members: PropTypes.array.isRequired,
+  ownerId: PropTypes.number,
+  isRequiredTargetValue: PropTypes.bool.isRequired,
+  onChange: PropTypes.func.isRequired,
+  changeKeyResultOwner: PropTypes.func.isRequired,
+  addMember: PropTypes.func.isRequired,
+  removeMember: PropTypes.func.isRequired,
+}
+
+export default KeyResultForm
--- app/javascript/components/keyresultmodal/KeyResultModal.jsx
+++ app/javascript/components/keyresultmodal/KeyResultModal.jsx
@@ -1,17 +1,8 @@
 import React, { Component } from 'react';
-import { findDOMNode } from 'react-dom';
 import { reduxForm } from 'redux-form'
-import { Field } from 'redux-form'
-import { Button, Form, Modal, TextArea } from 'semantic-ui-react'
-import KeyResultMemberSelect from '../form/KeyResultMemberSelect';
-import UserSelect from '../form/UserSelect';
-import OkrSelect from '../form/OkrSelect';
-import RequiredLabel from '../form/RequiredLabel';
-import { fromJS } from 'immutable';
+import { Button, Modal } from 'semantic-ui-react'
 import KeyResultSidebar from './KeyResultSidebar'
-import RenderField from '../form/RenderField'
-import RenderDateField from '../form/RenderDateField'
-import { validateKeyResultName, validateTargetValue, validateExpiredDate, normalizeExpiredDate } from '../../utils/validator'
+import KeyResultForm from './KeyResultForm'
 
 class KeyResultModal extends Component {
   constructor(props) {
@@ -19,24 +10,25 @@ class KeyResultModal extends Component {
     this.state = {
       members: [],
       ownerId: null,
+      description: '',
       isRequiredTargetValue: false,
     }
   }
 
-  changeKeyResultOwner(value) {
+  changeKeyResultOwner = value => {
     this.setState({
       ownerId: value,
     });
     this.removeMember(value);
   }
 
-  addMember(value) {
+  addMember = value => {
     const members = this.state.members;
     members.push(value);
     this.setState({ members });
   }
 
-  removeMember(value) {
+  removeMember = value => {
     const members = this.state.members;
     members.splice(members.indexOf(value), 1);
     this.setState({ members });
@@ -45,7 +37,7 @@ class KeyResultModal extends Component {
   save(validData) {
     const keyResult = {
       name: validData.name,
-      description: findDOMNode(this.refs.descriptionArea).value,
+      description: this.state.description,
       objectiveId: this.props.objective.get('id'),
       ownerId: this.state.ownerId,
       targetValue: validData.targetValue,
@@ -68,6 +60,7 @@ class KeyResultModal extends Component {
       this.setState({
         members: [],
         isRequiredTargetValue: false,
+        description: '',
       });
     }
 
@@ -83,7 +76,7 @@ class KeyResultModal extends Component {
 
   isEditing() {
     return this.props.dirty
-      || findDOMNode(this.refs.descriptionArea).value !== ''
+      || this.state.description !== ''
       || this.state.ownerId !== this.props.objective.get('owner').get('id')
       || this.state.members.length;
   }
@@ -121,101 +114,17 @@ class KeyResultModal extends Component {
         <Modal.Content>
           <div className="keyresult-modal__body">
             <KeyResultSidebar objective={objective} />
-            <div className="keyresult-modal__main">
-              <Form>
-                <Form.Group widths='equal'>
-                  <Form.Field>
-                    <RequiredLabel text='Key Result' />
-                    <Field
-                      name='name'
-                      type='text'
-                      component={RenderField}
-                      validate={[validateKeyResultName]}
-                    />
-                  </Form.Field>
-                </Form.Group>
-                <Form.Group widths='equal'>
-                  <Form.Field>
-                    <label>説明</label>
-                    <TextArea autoHeight rows={3} ref='descriptionArea'
-                              placeholder={`Key Result についての説明や補足を入力してください。\n説明を入力すると、メンバーに目指すべき方向性が伝わりやすくなります。`}
-                    />
-                  </Form.Field>
-                </Form.Group>
-                <Form.Group>
-                  <Form.Field>
-                    <div className="flex-start">
-                      <div style={{marginRight: "10px"}}>
-                        <RequiredLabel text='目標値' required={this.state.isRequiredTargetValue} />
-                        <div style={{width: "177px"}} >
-                          <Field
-                            name='targetValue'
-                            type='text'
-                            component={RenderField}
-                            validate={[validateTargetValue]}
-                          />
-                        </div>
-                      </div>
-                      <div>
-                        <label>単位</label>
-                        <Field
-                          name='valueUnit'
-                          type='text'
-                          placeholder='例：円、件、人'
-                          component={RenderField}
-                          onChange={(e, newValue) => this.setState({ isRequiredTargetValue: !!newValue })}
-                        />
-                      </div>
-                    </div>
-                  </Form.Field>
-                </Form.Group>
-                <Form.Group>
-                  <Form.Field>
-                    <RequiredLabel text='期限' />
-                    <Field
-                      name='expiredDate'
-                      component={RenderDateField}
-                      validate={[validateExpiredDate]}
-                      normalize={normalizeExpiredDate}
-                    />
-                  </Form.Field>
-                </Form.Group>
-                <Form.Group>
-                  <Form.Field>
-                    <RequiredLabel text='責任者' />
-                    <UserSelect
-                      users={this.props.users} 
-                      value={this.state.ownerId}
-                      onChange={value => this.changeKeyResultOwner(value)}
-                    />
-                  </Form.Field>
-                </Form.Group>
-                <Form.Group>
-                  <Form.Field>
-                    <label>関係者</label>
-                    <KeyResultMemberSelect
-                      users={this.props.users}
-                      members={this.state.members}
-                      excludedId={this.state.ownerId}
-                      add={this.addMember.bind(this)}
-                      remove={this.removeMember.bind(this)}
-                    />
-                  </Form.Field>
-                </Form.Group>
-                {objective && (
-                  <Form.Field>
-                    <RequiredLabel text='紐付く Objective' />
-                    <OkrSelect
-                      okrs={fromJS([objective])}
-                      value={objective.get('id')}
-                      preview={false}
-                      disabled={!!objective}
-                      onChange={value => {}}
-                    />
-                  </Form.Field>
-                )}
-              </Form>
-            </div>
+            <KeyResultForm
+              objective={objective}
+              users={this.props.users}
+              ownerId={this.state.ownerId}
+              members={this.state.members}
+              isRequiredTargetValue={this.state.isRequiredTargetValue}
+              onChange={values => this.setState({ ...values })}
+              changeKeyResultOwner={this.changeKeyResultOwner}
+              addMember={this.addMember}
+              removeMember={this.removeMember}
+            />
           </div>
         </Modal.Content>
         <Modal.Actions>
