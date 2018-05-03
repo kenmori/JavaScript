0a1ddd7417f4abd29d1ee4275ee876e27c15f978
rockwillj <rockwillj@gmail.com>
2018-05-03 21:51:43 +0900

    Modify: DatePicker で今日の日付をハイライト表示する

--- app/javascript/components/form/DatePicker.jsx
+++ app/javascript/components/form/DatePicker.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import moment from 'moment'
 import ReactDatePicker from 'react-datepicker';
 
 class DatePicker extends Component {
   render() {
     return (
       <div className='date-picker'>
-        <ReactDatePicker {...this.props} />
+        <ReactDatePicker {...this.props} highlightDates={[moment()]} />
       </div>
     )
   }
