1f5f14fc50162ad9f782964eff53ccde94aec8fc
rockwillj <rockwillj@gmail.com>
2018-05-06 15:18:42 +0900

    Add: why-did-you-update 導入 (デフォルトでは対象から全コンポーネントを除外しておく)

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,4 +13,8 @@ if (process.env.NODE_ENV !== 'production') {
   const Immutable = require('immutable')
   const installDevTools = require('immutable-devtools')
   installDevTools(Immutable);
+
+  const React = require('react')
+  const { whyDidYouUpdate } = require('why-did-you-update')
+  whyDidYouUpdate(React, { exclude: /./ }) // デフォルトでは対象から全コンポーネントを除外しておく
 }
