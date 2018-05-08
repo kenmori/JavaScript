c4d59743bbcc2577fd087af2fb5b2cac503393da
rockwillj <rockwillj@gmail.com>
2018-05-08 17:40:56 +0900

    Modify: コンテナーで state から O 並び替えが可能か判定しているコードを reselect の selector で書き換える

--- app/javascript/utils/okr.js
+++ app/javascript/utils/okr.js
@@ -1,22 +1,5 @@
 import avatar_image from '../images/avatar.png';
 
-// Objective 並び替えが可能かどうか
-// - 選択ユーザー = ログインユーザー = Objective 責任者 => true
-// - その他 => false
-// ※ 選択ユーザーと Objective 一覧の更新タイミングのずれを吸収するために用いる
-export const canMoveObjective = state => {
-  const currentUserId = state.current.get('userId');
-  const loginUserId = state.loginUser.get('id');
-  if (currentUserId === loginUserId) {
-    const objective = state.entities.objectives.get(state.objectives.get('ids').first());
-    if (objective) {
-      const objectiveOwnerId = objective.get('owner').get('id');
-      return currentUserId === objectiveOwnerId;
-    }
-  }
-  return false;
-}
-
 // O/KR 選択ドロップダウンに指定する O/KR 一覧データを返す
 export const okrOptions = (okrs, withNone) => {
   let options = okrs.map(okr => ({
--- app/javascript/utils/selector.js
+++ app/javascript/utils/selector.js
@@ -95,3 +95,24 @@ export const getObjectiveCandidates = createSelector(
     return candidates
   }
 )
+
+// Objective 並び替えが可能かどうか
+// - 選択ユーザー = ログインユーザー = Objective 責任者 => true
+// - その他 => false
+// ※ 選択ユーザーと Objective 一覧の更新タイミングのずれを吸収するために用いる
+export const canMoveObjective = createSelector(
+  state => state.current.get('userId'),
+  state => state.loginUser.get('id'),
+  state => state.objectives.get('ids'),
+  state => state.entities,
+  (currentUserId, loginUserId, objectiveIds, entities) => {
+    if (currentUserId === loginUserId) {
+      const objective = entities.objectives.get(objectiveIds.first())
+      if (objective) {
+        const objectiveOwnerId = objective.get('owner').get('id')
+        return currentUserId === objectiveOwnerId
+      }
+    }
+    return false
+  }
+)
