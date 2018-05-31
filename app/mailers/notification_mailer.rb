5c6458d91ff59b75f480419b9a3605a18944fed3
rockwillj <rockwillj@gmail.com>
2018-05-31 18:52:01 +0900

    Modify: 通知メール：アプリ URL を url_for で指定する

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -4,6 +4,7 @@ class NotificationMailer < ApplicationMailer
     @assignor = "#{assignor.last_name} #{assignor.first_name}"
     @assignee = "#{assignee.last_name} #{assignee.first_name}"
     @key_result = key_result
+    @url = url_for(controller: 'home')
     mail to: assignee.email,
          subject: 'Resily 新しい OKR が割当てられました'
   end
