661fa86e251109b33bd8cb9fab02838d30fc60f9
rockwillj <rockwillj@gmail.com>
2018-05-31 21:16:49 +0900

    Update: Active Job のアダプターをデフォルトの :async から :inline に変更する (暫定)
    - :async は動作しないため (@see https://stackoverflow.com/a/38318610)
    - 次の対応で Resque, Sidekiq, Delayed Job のいずれかに切り替える

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,6 +34,9 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
 
+  # Use a real queuing backend for Active Job (and separate queues per environment)
+  config.active_job.queue_adapter     = :inline
+
   config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
-  # config.active_job.queue_adapter     = :resque
+  config.active_job.queue_adapter     = :inline
   # config.active_job.queue_name_prefix = "resily_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
