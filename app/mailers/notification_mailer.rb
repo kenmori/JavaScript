89efb8f51b049c6fea98dc31cf4d06029231d364
rockwillj <rockwillj@gmail.com>
2018-05-31 13:38:39 +0900

    Modify: 通知メールの件名と本文をダミーから適切な内容に変更する

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,7 +1,9 @@
 class NotificationMailer < ApplicationMailer
 
-  def assign_key_result
-    @greeting = "Hi"
-    mail to: "to@example.com", subject: 'Resily'
+  def assign_key_result(assignee, key_result)
+    @assignee = "#{assignee.last_name} #{assignee.first_name}"
+    @key_result = key_result
+    mail to: assignee.email,
+         subject: 'Resily 新しい OKR が割当てられました'
   end
 end
