1bab5b1475ebf081628189b00f001fbc063260ae
rockwillj <rockwillj@gmail.com>
2018-06-04 13:49:03 +0900

    Modify: 組織情報表示スクリプト：O/KR の平均進捗率を出力する

--- lib/scripts/print_organizations.rb
+++ lib/scripts/print_organizations.rb
@@ -10,7 +10,16 @@ class PrintOrganizations
       puts "  - #{users.count} users (#{users.where(disabled: false).count} enabled, #{users.where(disabled: true).count} disabled)"
 
       organization.okr_periods.each do |okr_period|
-        puts "  - #{okr_period.objectives.count} objectives, #{okr_period.key_results.count} key results (#{okr_period.name})"
+        objective_count = okr_period.objectives.count
+        objective_progress_rate_average = objective_count == 0 ? '-'
+            : okr_period.objectives.reduce(0) { |sum, objective| sum + objective.progress_rate } / objective_count
+        key_result_count = okr_period.key_results.count
+        key_result_progress_rate_average = key_result_count == 0 ? '-'
+            : okr_period.key_results.reduce(0) { |sum, key_result| sum + key_result.progress_rate } / key_result_count
+
+        puts "  - [#{okr_period.name}]"
+        puts "    - #{objective_count} objectives (average progress rate: #{objective_progress_rate_average}%)"
+        puts "    - #{key_result_count} key results (average progress rate: #{key_result_progress_rate_average}%)"
       end
     end
     puts ''
