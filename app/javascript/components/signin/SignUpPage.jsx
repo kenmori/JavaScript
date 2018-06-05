b2c9f4adf5d231684a8427dfa5b1f68b15fef6b0
rockwillj <rockwillj@gmail.com>
2018-06-05 19:43:55 +0900

    Modify: 新規ユーザー登録画面のデザインを大幅に見直す (中央寄せ、Segment、開始日/終了日→ OKR 期間、Divider → Form.Group、アイコン表示など)

--- app/javascript/components/signin/SignUpPage.jsx
+++ app/javascript/components/signin/SignUpPage.jsx
@@ -1,6 +1,6 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types'
-import { Button, Form, Input, Select, Image, Divider } from 'semantic-ui-react';
+import { Button, Form, Input, Image, Segment } from 'semantic-ui-react';
 import moment from 'moment';
 import logo_image from '../../images/logo_large.png';
 import DatePicker from '../form/DatePicker';
@@ -48,70 +48,73 @@ class SignUpPage extends PureComponent {
   render() {
     return (
       <div className="sign-up">
-        <main className="center">
-          <Image as="h1" src={logo_image} title="Resily" />
-          <Form className="user-form">
-            <Form.Group className="text-input-group">
+        <Image as="h1" src={logo_image} title="Resily"/>
+
+        <Segment raised compact padded="very">
+          <Form>
+            <Form.Group>
+              <Form.Input
+                inline
+                label="組織名"
+                name="organization"
+                autoComplete="organization"
+                placeholder="会社名やチーム名など"
+                icon="building"
+                iconPosition="left"
+                ref={node => { this.organizationInput = node }}
+              />
+            </Form.Group>
+
+            <Form.Group>
               <Form.Field inline>
-                <div>組織名</div>
+                <label>管理者</label>
                 <Input
-                  name="organization"
-                  autoComplete="organization"
-                  size="mini"
-                  placeholder="会社名やチーム名など"
-                  ref={(node) => { this.organizationInput = node; }}
-                />
-              </Form.Field>
-              <Divider hidden />
-              <Form.Field inline>
-                <div>管理者</div>
-                <Input
+                  className="last-name"
                   name="family-name"
                   autoComplete="family-name"
-                  className="last-name"
-                  size="mini"
                   placeholder="姓"
-                  ref={(node) => { this.lastNameInput = node; }}
+                  ref={node => { this.lastNameInput = node }}
                 />
                 <Input
+                  className="first-name"
                   name="given-name"
                   autoComplete="family-name"
-                  className="first-name"
-                  size="mini"
                   placeholder="名"
-                  ref={(node) => { this.firstNameInput = node; }}
+                  ref={node => { this.firstNameInput = node }}
                 />
               </Form.Field>
+              <Form.Input
+                inline
+                label="メールアドレス"
+                type="email"
+                name="email"
+                autoComplete="email"
+                placeholder="name@example.com"
+                icon="mail"
+                iconPosition="left"
+                ref={node => { this.emailInput = node }}
+              />
+              <Form.Input
+                inline
+                label="パスワード"
+                type="password"
+                name="current-password"
+                autoComplete="current-password"
+                placeholder="英数字8文字以上"
+                icon="lock"
+                iconPosition="left"
+                ref={node => { this.passwordInput = node }}
+              />
+            </Form.Group>
+
+            <Form.Group>
               <Form.Field inline>
-                <div>メールアドレス</div>
-                <Input
-                  type="email"
-                  name="email"
-                  autoComplete="email"
-                  size="mini"
-                  placeholder="name@example.com"
-                  ref={(node) => { this.emailInput = node; }}
-                />
-              </Form.Field>
-              <Form.Field inline>
-                <div>パスワード</div>
-                <Input
-                  type="password"
-                  name="current-password"
-                  autoComplete="current-password"
-                  size="mini"
-                  placeholder="英数字8文字以上"
-                  ref={(node) => { this.passwordInput = node; }}
-                />
-              </Form.Field>
-              <Divider hidden />
-              <Form.Field inline>
-                <div>開始日</div>
+                <label>OKR 期間</label>
                 <DatePicker
                   dateFormat="YYYY/M/D"
                   locale="ja"
                   selected={this.state.startDate}
-                  onChange={(date) => {
+                  onChange={date => {
                     if (this.state.endDateChanged) {
                       this.setState({
                         startDate: date
@@ -121,56 +124,46 @@ class SignUpPage extends PureComponent {
                       this.setState({
                         startDate: date,
                         endDate: this.getEndDate(date, this.state.okrSpan),
-                      });
+                      })
                     }
                   }}
                 />
-              </Form.Field>
-              <Form.Field inline>
-                <div>終了日</div>
+                <span className="between">-</span>
                 <DatePicker
                   dateFormat="YYYY/M/D"
                   locale="ja"
                   selected={this.state.endDate}
-                  onChange={(date) => {
+                  onChange={date => this.setState({ endDate: date, endDateChanged: true })}
+                />
+              </Form.Field>
+              <Form.Select
+                inline
+                label="OKR 周期"
+                defaultValue={this.state.okrSpan}
+                options={[
+                  { key: 1, value: 1, text: '1ヶ月間' },
+                  { key: 3, value: 3, text: '3ヶ月間' },
+                  { key: 6, value: 6, text: '半年間' },
+                  { key: 12, value: 12, text: '1年間' }
+                ]}
+                onChange={(event, data) => {
+                  const okrSpan = data.value
+                  if (this.state.endDateChanged) {
+                    this.setState({ okrSpan })
+                  } else {
+                    // 終了日をユーザーが変更していない場合、計算し直す
                     this.setState({
-                      endDate: date,
-                      endDateChanged: true
+                      okrSpan,
+                      endDate: this.getEndDate(this.state.startDate, okrSpan),
                     })
-                  }}
-                />
-              </Form.Field>
-              <Form.Field inline>
-                <div>OKR 周期</div>
-                <Select
-                  defaultValue={this.state.okrSpan}
-                  options={[
-                    { key: 1, value: 1, text: '1ヶ月間' },	
-                    { key: 3, value: 3, text: '3ヶ月間' },	
-                    { key: 6, value: 6, text: '半年間' },	
-                    { key: 12, value: 12, text: '1年間' }
-                  ]}
-                  onChange={(event, data) => {
-                    const okrSpan = data.value;
-                    if (this.state.endDateChanged) {
-                      this.setState({ okrSpan });
-                    } else {
-                      // 終了日をユーザーが変更していない場合、計算し直す
-                      this.setState({
-                        okrSpan,
-                        endDate: this.getEndDate(this.state.startDate, okrSpan),
-                      });
-                    }
-                  }}
-                />
-              </Form.Field>
+                  }
+                }}
+              />
             </Form.Group>
-            <Divider hidden />
-            <div>
-              <Button positive onClick={this.addOrganization}>登録する</Button>
-            </div>
           </Form>
-        </main>
+        </Segment>
+
+        <Button className="submit" positive onClick={this.addOrganization}>登録する</Button>
       </div>
     );
   }
