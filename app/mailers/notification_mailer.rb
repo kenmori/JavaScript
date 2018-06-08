e8feb71df6be95be478d7e3fee24d18306902e39
rockwillj <rockwillj@gmail.com>
2018-06-08 14:41:58 +0900

    Refactor: KR 割り当てメールの通知条件をコールバック内ではなく Mailer 側で判定する

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,11 +1,15 @@
 class NotificationMailer < ApplicationMailer
 
-  def assign_key_result(assignor, assignee, key_result)
-    @assignor = "#{assignor.last_name} #{assignor.first_name}"
-    @assignee = "#{assignee.last_name} #{assignee.first_name}"
+  def assign_key_result(current_user, user, key_result)
+    return unless current_user
+    return if current_user.id == user.id
+
+    @assignor = "#{current_user.last_name} #{current_user.first_name}"
+    @assignee = "#{user.last_name} #{user.first_name}"
     @key_result = key_result
     @url = url_for(controller: 'home')
-    mail to: assignee.email,
+
+    mail to: user.email,
          subject: 'Resily 新しい OKR が割当てられました'
   end
 end
