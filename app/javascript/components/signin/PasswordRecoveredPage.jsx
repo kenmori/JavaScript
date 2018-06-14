c415f83a2bba455211b3555005cc121ec001ead0
rockwillj <rockwillj@gmail.com>
2018-06-14 20:00:19 +0900

    modify: パスワードリセット完了画面に新規ユーザー登録画面のデザインを適用する

--- app/javascript/components/signin/PasswordRecoveredPage.jsx
+++ app/javascript/components/signin/PasswordRecoveredPage.jsx
@@ -1,19 +1,21 @@
 import React, { PureComponent } from 'react';
-import { Image } from 'semantic-ui-react';
+import { Image, Segment, Message } from 'semantic-ui-react';
 import logo_image from '../../images/logo_large.png';
 
 class PasswordRecoveredPage extends PureComponent {
   render() {
     return (
-      <div className="password-recovered">
-        <main className="main-content">
-          <Image as='h1' src={logo_image} title='Resily' />
-          <p>
-            入力されたメールアドレスにメールを送信しました。<br />
-            メールが届かない場合はアドレスを確かめて送信し直してください。
-          </p>
+      <div className="password-recover completed">
+        <Image as="h1" src={logo_image} title="Resily" />
+
+        <Segment raised compact padded="very">
+          入力されたメールアドレスにメールを送信しました。<br />
+          メールが届かない場合はアドレスを確かめて送信し直してください。
+        </Segment>
+
+        <Message className="password-recover__link">
           <p><a href="/">トップに戻る</a></p>
-        </main>
+        </Message>
       </div>
     );
   }
