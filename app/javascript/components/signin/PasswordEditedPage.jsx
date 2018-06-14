0d9aa6e2e73606bee326904d2b0b54761007ba48
rockwillj <rockwillj@gmail.com>
2018-06-14 20:12:58 +0900

    modify: パスワード設定完了画面に新規ユーザー登録画面のデザインを適用する

--- app/javascript/components/signin/PasswordEditedPage.jsx
+++ app/javascript/components/signin/PasswordEditedPage.jsx
@@ -1,16 +1,20 @@
 import React, { PureComponent } from 'react';
-import { Image } from 'semantic-ui-react';
+import { Image, Segment, Message } from 'semantic-ui-react';
 import logo_image from '../../images/logo_large.png';
 
 class PasswordEditedPage extends PureComponent {
   render() {
     return (
-      <div className="password-edited">
-        <main className="main-content">
-          <Image as='h1' src={logo_image} title='Resily' />
-          <p>アカウントに新しいパスワードを再設定しました。</p>
+      <div className="password-edit complete">
+        <Image as="h1" src={logo_image} title="Resily" />
+
+        <Segment raised compact padded="very">
+          アカウントに新しいパスワードを再設定しました。
+        </Segment>
+
+        <Message className="password-edit__link">
           <p><a href="/">トップに戻る</a></p>
-        </main>
+        </Message>
       </div>
     );
   }
