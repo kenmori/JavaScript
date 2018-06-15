f6a094c060e85ad6036ed6faa7591bad0183d1d7
rockwillj <rockwillj@gmail.com>
2018-06-15 14:10:45 +0900

    modify: パスワード設定画面：クエリ文字列のパースを querystring で処理する (自力ではやらない)

--- app/javascript/containers/PasswordEditPage.jsx
+++ app/javascript/containers/PasswordEditPage.jsx
@@ -2,10 +2,11 @@ import PasswordEditPage from '../components/signin/PasswordEditPage';
 import { connect } from 'react-redux';
 import usersActions from '../actions/users';
 import dialogActions from '../actions/dialogs'
+import queryString from 'querystring'
 
 const mapStateToProps = (state, { location }) => {
   return {
-    token: location.search.split('reset_password_token=')[1].split('=')[0],
+    token: queryString.parse(location.search.replace('?', '')).reset_password_token,
     passwordEditedPath: '/users/password/edit/completed',
     isEdited: state.password.get('isEdited'),
   };
