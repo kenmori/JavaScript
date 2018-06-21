6abdd78aa116fbade95f211bb20d7b59f2a8b6ed
rockwillj <rockwillj@gmail.com>
2018-06-21 17:38:59 +0900

    modify: ユーザー追加フォームの refs を state に書き換える (付随的にローディング中に管理者チェックボックスが外れる現象が直る)

--- app/javascript/components/setting/UserSettingTab.jsx
+++ app/javascript/components/setting/UserSettingTab.jsx
@@ -9,35 +9,45 @@ class UserSettingTab extends PureComponent {
   constructor(props) {
     super(props);
     this.state = {
+      firstName: '',
+      lastName: '',
+      email: '',
+      admin: false,
       keyword: '',
       showDisabledUsers: false,
     };
   }
 
   addUser = () => {
+    const { firstName, lastName, email, admin } = this.state
     this.props.confirm({
-      content: `${this.refs.email.inputRef.value} に確認メールを送信します。メール中の URL がクリックされると処理が完了します。ユーザーを追加しますか？`,
+      content: `${email} に確認メールを送信します。メール中の URL がクリックされると処理が完了します。ユーザーを追加しますか？`,
       onConfirm: () => {
-        this.props.addUser({
-          firstName: this.refs.firstName.inputRef.value,
-          lastName: this.refs.lastName.inputRef.value,
-          email: this.refs.email.inputRef.value,
-          admin: this.refs.admin.inputRef.checked,
-        });
-        this.refs.lastName.inputRef.value = '';
-        this.refs.firstName.inputRef.value = '';
-        this.refs.email.inputRef.value = '';
-        this.refs.admin.inputRef.checked = false;
+        this.props.addUser({ firstName, lastName, email, admin })
+        this.setState({
+          firstName: '',
+          lastName: '',
+          email: '',
+        })
       },
     });
   };
 
-  handleKeywordChange = (event, { value }) => this.setState({ keyword: value })
+  handleFirstNameChange = (e, { value }) => this.setState({ firstName: value })
+
+  handleLastNameChange = (e, { value }) => this.setState({ lastName: value })
+
+  handleEmailChange = (e, { value }) => this.setState({ email: value })
+
+  handleAdminChange = (e, { checked }) => this.setState({ admin: checked })
+
+  handleKeywordChange = (e, { value }) => this.setState({ keyword: value })
 
   handleShowDisabledUsersClick = () => this.setState({ showDisabledUsers: true })
 
   render() {
     if (this.props.users.size === 0) return null;
+    const { firstName, lastName, email, admin } = this.state
     const enabledUsers = this.props.users.filter(user => !user.get('disabled'));
     const disabledUsers = this.props.users.filter(user => user.get('disabled'));
     return (
@@ -46,16 +56,16 @@ class UserSettingTab extends PureComponent {
           <Table.Body>
             <Table.Row>
               <Table.Cell>
-                <Input type="text" maxLength="255" required ref='lastName' placeholder="姓" />
+                <Input value={lastName} maxLength="255" placeholder="姓" onChange={this.handleLastNameChange} />
               </Table.Cell>
               <Table.Cell>
-                <Input type="text" maxLength="255" required ref='firstName' placeholder="名" />
+                <Input value={firstName} maxLength="255" placeholder="名" onChange={this.handleFirstNameChange} />
               </Table.Cell>
               <Table.Cell>
-                <Input type="email" maxLength="255" required ref='email' placeholder="name@example.com" />
+                <Input type="email" value={email} maxLength="255" placeholder="name@example.com"  onChange={this.handleEmailChange} />
               </Table.Cell>
               <Table.Cell>
-                <Checkbox label='管理者' required ref='admin' />
+                <Checkbox label='管理者' checked={admin} onChange={this.handleAdminChange} />
               </Table.Cell>
               <Table.Cell textAlign="center">
                 <Button icon="plus" content="追加する" onClick={this.addUser}/>
@@ -64,9 +74,7 @@ class UserSettingTab extends PureComponent {
           </Table.Body>
         </Table>
 
-        <Input icon="search" placeholder="ユーザーを検索&#8230;"
-               onChange={this.handleKeywordChange}
-        />
+        <Input icon="search" placeholder="ユーザーを検索&#8230;" onChange={this.handleKeywordChange} />
 
         <UsersTable
           users={enabledUsers}
