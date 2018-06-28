59f42ed92fce2a8a914ad45bb686cd0b9bbc91c3
rockwillj <rockwillj@gmail.com>
2018-06-28 16:16:13 +0900

    remove: 選択ユーザーと O 一覧の更新タイミングのずれを吸収していた canMoveObjective を削除 (代わりに同期化されたユーザー ID を用いる)

--- app/javascript/containers/ObjectiveList.jsx
+++ app/javascript/containers/ObjectiveList.jsx
@@ -2,13 +2,12 @@ import ObjectiveList from '../components/dashboard/ObjectiveList';
 import { connect } from 'react-redux';
 import objectiveActions from '../actions/objectives';
 import objectiveOrderActions from '../actions/objectiveOrders';
-import { canMoveObjective } from "../utils/selector";
 
 const mapStateToProps = (state) => {
   return {
     selectedObjectiveId: state.objectives.getIn(['selectedOkr', 'objectiveId']),
     objectiveOrder: state.objectives.get('ids'),
-    canMoveObjective: canMoveObjective(state),
+    canMoveObjective: state.loginUser.get('id') === state.current.get('userIdAtFetchedObjectives'),
   };
 };
 
