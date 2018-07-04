dbcc5926d31213e7751dc9785d949cdb3032b675
rockwillj <rockwillj@gmail.com>
2018-07-04 17:21:08 +0900

    modify: 最初のユーザーを代表者にする処理をアカウント作成時から OrganizationMember の before_create コールバックに移す (seed 環境でも適用されるように)

--- app/models/organization_member.rb
+++ app/models/organization_member.rb
@@ -3,4 +3,8 @@ class OrganizationMember < ApplicationRecord
   belongs_to :user
 
   enum role: { owner: 0, member: 1 }
+
+  before_create do
+    self.role = :owner unless organization.organization_members.exists? # 最初のユーザーを代表者にする
+  end
 end
