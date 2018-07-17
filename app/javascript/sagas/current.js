e0731cccde62179172d64e01f953246f280db97b
rockwillj <rockwillj@gmail.com>
2018-07-17 17:22:39 +0900

    modify: 選択したマップ OKR の KR 一覧に未 fetch の子 O が紐付いている場合は fetch する

--- app/javascript/sagas/current.js
+++ app/javascript/sagas/current.js
@@ -19,9 +19,22 @@ function* selectUser({ payload }) {
   yield put(objectiveActions.fetchOkrs(okrPeriodId, userId, false))
 }
 
+function* selectMapOkr({ payload }) {
+  const { objectiveId, keyResultIds } = payload
+  // 選択したマップ OKR の KR 一覧に未 fetch の子 O が紐付いている場合は fetch する
+  const isFetched = yield select(state => keyResultIds.every(keyResultId => {
+    const keyResult = state.entities.keyResults.get(keyResultId)
+    return keyResult.get('childObjectiveIds').every(childObjectiveId => state.entities.objectives.has(childObjectiveId))
+  }))
+  if (!isFetched) {
+    yield put(objectiveActions.fetchObjective(objectiveId)) // with loading
+  }
+}
+
 export function* currentSagas() {
   yield all([
     takeLatest(ActionTypes.SELECT_OKR_PERIOD, selectOkrPeriod),
     takeLatest(ActionTypes.SELECT_USER, selectUser),
+    takeLatest(ActionTypes.SELECT_MAP_OKR, selectMapOkr),
   ])
 }
