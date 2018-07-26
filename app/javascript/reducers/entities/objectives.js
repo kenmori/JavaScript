18d4895860ff09c8b03949cc0d9a683e588fbafa
rockwillj <rockwillj@gmail.com>
2018-07-26 20:53:15 +0900

    fix: 紐付け残件：O 削除時に、上位 KR = 削除した O に紐付く KR な下位 O がある場合は上位 KR を null にリセットする

--- app/javascript/reducers/entities/objectives.js
+++ app/javascript/reducers/entities/objectives.js
@@ -27,6 +27,13 @@ export default handleActions({
     [ActionTypes.ADDED_KEY_RESULT]: merge,
     [ActionTypes.UPDATED_KEY_RESULT]: merge,
     [ActionTypes.REMOVED_KEY_RESULT]: merge,
+    [ActionTypes.REMOVED_KEY_RESULT_IDS]: (state, { payload }) => {
+      const { keyResultIds } = payload
+      // 上位 KR = 削除した KR な下位 O がある場合は上位 KR を null にリセットする 
+      const childObjectives = state.filter(objective => keyResultIds.includes(objective.get('parentKeyResultId')))
+      return childObjectives.isEmpty() ? state
+        : state.merge(childObjectives.map(objective => objective.set('parentKeyResultId', null)))
+    },
   },
   Map()
 );
