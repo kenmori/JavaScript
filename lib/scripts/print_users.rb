b58e521db32bd80ac7358ae29b44206086e0a40d
rockwillj <rockwillj@gmail.com>
2018-07-27 17:48:11 +0900

    modify: ユーザー情報スクリプト：O/KR 数 (タスク KR 数) を出力する

--- lib/scripts/print_users.rb
+++ lib/scripts/print_users.rb
@@ -20,7 +20,12 @@ class PrintUsers
     index = 0
     organization.users.each do |user|
       index += 1
+      objective_count = user.objectives.count
+      key_result_count = user.key_results.count
+      unprocessed_count = user.unprocessed_key_results.count
       puts "#{index}. #{user.last_name} #{user.first_name} (#{user.email})"
+      puts "   - #{objective_count} objectives"
+      puts "   - #{key_result_count} key results (#{unprocessed_count} unprocessed)"
       puts "   - created at #{user.created_at.strftime('%Y-%m-%d')}"
       puts "   - sign in at #{user.sign_in_at.strftime('%Y-%m-%d %H:%M')}" if user.sign_in_at
       puts "   - disabled at #{user.disabled_at.strftime('%Y-%m-%d')}" if user.disabled_at
