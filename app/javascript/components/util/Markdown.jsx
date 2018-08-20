9abdb709763b9877f8cc6c6ae56b435d78a80f7a
rockwillj <rockwillj@gmail.com>
2018-08-20 19:04:54 +0900

    add: remark-breaks (改行を br に変換) , remark-external-links (外部リンクに target 付与)

--- app/javascript/components/util/Markdown.jsx
+++ app/javascript/components/util/Markdown.jsx
@@ -3,20 +3,21 @@ import PropTypes from 'prop-types';
 import remark from 'remark';
 import reactRemark from 'remark-react'
 import emoji from 'remark-emoji'
+import breaks from 'remark-breaks'
+import externalLinks from 'remark-external-links'
 
 class Markdown extends PureComponent {
 
   render() {
     const options = {
-      gfm: true,
-      pedantic: true,
-      breaks: true,
-      sanitize: false,
+      sanitize: false, // to use GFM task list (https://github.com/mapbox/remark-react/issues/44) 
     }
     return (
       <div className="markdown markdown-body">
         {remark()
           .use(emoji)
+          .use(breaks)
+          .use(externalLinks)
           .use(reactRemark, options)
           .processSync(this.props.text)
           .contents}
