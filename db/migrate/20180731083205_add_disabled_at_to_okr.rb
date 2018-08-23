27e89d3a8cca7dd9b720e69a477cac5764845a84
rockwillj <rockwillj@gmail.com>
2018-08-23 15:45:25 +0900

    fix: typo (DisableAt => DisabledAt)

--- db/migrate/20180731083205_add_disabled_at_to_okr.rb
+++ db/migrate/20180731083205_add_disabled_at_to_okr.rb
@@ -1,4 +1,4 @@
-class AddDisableAtToOkr < ActiveRecord::Migration[5.1]
+class AddDisabledAtToOkr < ActiveRecord::Migration[5.1]
   def change
     add_column :objectives, :disabled_at, :datetime
     add_column :key_results, :disabled_at, :datetime
