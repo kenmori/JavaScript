7d1ee66e642cd5f3f6dfb9e438ecf488d02cc0b2
neko-neko <seiichi.nishikata456@gmail.com>
2018-09-01 20:54:59 +0900

    Fix definition order of DefinePlugin

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,6 +1,11 @@
 const environment = require('./environment')
 const webpack = require('webpack')
 
+environment.plugins.set('Define', new webpack.DefinePlugin({
+  'process.env.NODE_ENV': JSON.stringify('production'),
+  'process.env.GA_TRACKING_CODE': JSON.stringify('UA-111410984-3'),
+}))
+
 environment.plugins.set('UglifyJs', new webpack.optimize.UglifyJsPlugin({
   parallel:   true,
   sourceMap:  false,
@@ -14,11 +19,6 @@ environment.plugins.set('UglifyJs', new webpack.optimize.UglifyJsPlugin({
   output: {
     comments: false
   }
-}))
-
-environment.plugins.set('Define', new webpack.DefinePlugin({
-  'process.env.NODE_ENV': JSON.stringify('production'),
-  'process.env.GA_TRACKING_CODE': JSON.stringify('UA-111410984-3'),
 }))
 
 const config = environment.toWebpackConfig()
