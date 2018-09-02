6b2a3f2affb0e4d91a64aaaeb6232e4213a346c1
neko-neko <seiichi.nishikata456@gmail.com>
2018-09-02 22:39:32 +0900

    Add uglify setting

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -6,6 +6,21 @@ environment.plugins.append('DefinePlugin', new webpack.DefinePlugin({
   'process.env.GA_TRACKING_CODE': JSON.stringify('UA-111410984-3'),
 }))
 
+environment.plugins.append("UglifyJs", new webpack.optimize.UglifyJsPlugin({
+  parallel: true,
+  sourceMap: false,
+  mangle: false,
+  uglifyOptions: {
+    mangle: false
+  },
+  compress: {
+    warnings: false
+  },
+  output: {
+    comments: false
+  }
+}))
+
 const config = environment.toWebpackConfig()
 config.devtool = 'eval'
 module.exports = config
