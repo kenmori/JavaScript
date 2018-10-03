f6a51f308e0bf38c5b20ba903e2e28a8ad843a09
kbaba1001 <kbaba1001@users.noreply.github.com>
2018-10-03 17:19:41 +0900

    remove rescue_from

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,40 +8,10 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
   helper_method :current_organization
 
-  rescue_from ActionController::RoutingError, with: :not_found
-  rescue_from ActiveRecord::RecordNotFound, with: :not_found
-  rescue_from ActionController::InvalidAuthenticityToken, with: :unprocessable_entity
-
   before_action :authenticate_user!
   around_action :set_current_user
 
   protected
-
-    # render 400
-    def bad_request(message = "無効な操作です")
-      render_with_error(:bad_request, message)
-    end
-
-    # render 403
-    def forbidden(message = "許可されていない操作です")
-      render_with_error(:forbidden, message)
-    end
-
-    # render 404
-    def not_found(message = "操作の対象が存在しません")
-      render_with_error(:not_found, message)
-    end
-
-    # render 422
-    def unprocessable_entity(message = "正常に処理できません")
-      render_with_error(:unprocessable_entity, message)
-    end
-
-    # render 422 with errors
-    def unprocessable_entity_with_errors(errors)
-      render_with_errors(:unprocessable_entity, errors)
-    end
-
     #  current_user return decorated devise current_user object
     def current_user
       ActiveDecorator::Decorator.instance.decorate(super) if super.present?
@@ -74,20 +44,6 @@ class ApplicationController < ActionController::Base
     end
 
   private
-
-    # render_with_error is render json of error.
-    def render_with_error(code, message)
-      # TODO: structureの定義を見直す
-      render json: { error: message }, status: code
-    end
-
-    # render_with_errors is render json of ActiveModel::Errors.
-    def render_with_errors(code, errors)
-      # TODO: クライアントとどうエラーのやり取りを行うか考える
-      # errors.messageを展開するだけであれば展開した文字列を引数として
-      # 直接renderせずにrender_with_errorを呼び出す方式に変える
-      render json: { error: errors }, status: code
-    end
 
     # Devise の current_user に Model からアクセスするための hack
     # @see https://stackoverflow.com/a/2513456
