a0715e714244c0e2a9270af02d7eefd59a13c29b
kbaba1001 <kbaba1001@users.noreply.github.com>
2018-10-11 13:55:10 +0900

    request spec の雛形を用意

---
+++ spec/requests/key_results_spec.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+RSpec.describe "key results", type: :request do
+  #index
+  describe 'GET /key_results' do
+  end
+
+  #index_candidates
+  describe 'GET /key_results/candidates' do
+  end
+
+  #index_unprocessed
+  describe 'GET /key_results/unprocessed' do
+  end
+
+  #show_objective
+  describe 'GET /key_results/:id/objective' do
+  end
+
+  #create
+  describe 'POST /key_results' do
+  end
+
+  #update
+  describe 'PATCH /key_results/:id' do
+  end
+
+  #update_disabled
+  describe 'PUT /key_results/:id/disable' do
+  end
+
+  #destroy
+  describe 'DELETE /key_results/:id' do
+  end
+
+  #update_processed
+  describe 'PUT /key_results/:id/process' do
+  end
+
+  #comment_labels
+  describe 'GET /key_results/comment_labels' do
+  end
+end
