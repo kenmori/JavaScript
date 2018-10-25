607e33acc0dd2b10851e3fef2e3cd0c508e79026
kbaba1001 <kbaba1001@users.noreply.github.com>
2018-10-25 16:14:41 +0900

    add department model

---
+++ app/models/department.rb
@@ -0,0 +1,24 @@
+# ## Schema Information
+#
+# Table name: `departments`
+#
+# ### Columns
+#
+# Name                     | Type               | Attributes
+# ------------------------ | ------------------ | ---------------------------
+# **`id`**                 | `bigint(8)`        | `not null, primary key`
+# **`ancestry`**           | `string(255)`      | `not null`
+# **`display_order`**      | `integer`          | `not null`
+# **`name`**               | `string(255)`      | `not null`
+# **`soft_destroyed_at`**  | `datetime`         |
+# **`created_at`**         | `datetime`         | `not null`
+# **`updated_at`**         | `datetime`         | `not null`
+# **`organization_id`**    | `bigint(8)`        | `not null`
+#
+
+class Department < ApplicationRecord
+  has_ancestry
+  soft_deletable
+
+  belongs_to :organization
+end
