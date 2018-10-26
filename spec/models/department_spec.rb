3d0b4e95c8b625009c6c83aefe3d6e43dde1aa49
kbaba1001 <kbaba1001@users.noreply.github.com>
2018-10-26 17:15:17 +0900

    Department association spec

--- spec/models/department_spec.rb
+++ spec/models/department_spec.rb
@@ -1,5 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe Department, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "associations" do
+    let!(:organization) { OrganizationFactory.new.create }
+    let!(:admin_user) { UserFactory.new(organization: organization).create(admin: true) }
+    let!(:member_user_1) {
+      UserFactory.new(organization: organization).create(
+        email: "member_user_1@example.com",
+        admin: false
+      )
+    }
+    let!(:member_user_2) {
+      UserFactory.new(organization: organization).create(
+        email: "member_user_2@example.com",
+        admin: false
+      )
+    }
+    let!(:department_fc) { DepartmentFactory.new(organization: organization) }
+    let!(:department) { department_fc.create }
+
+    before do
+      department_fc.add_user(user: admin_user, role: :owner)
+      department_fc.add_user(user: member_user_1, role: :member)
+      department_fc.add_user(user: member_user_2, role: :member)
+    end
+
+    example "#users" do
+      expect(department.users).to contain_exactly(admin_user, member_user_1, member_user_2)
+    end
+
+    example "#owner" do
+      expect(department.owner).to eq(admin_user)
+    end
+
+    example "#member" do
+      expect(department.members).to contain_exactly(member_user_1, member_user_2)
+    end
+  end
 end
