a31b76067249945051affa637406b54e030bcc4a
kbaba1001 <kbaba1001@users.noreply.github.com>
2018-11-03 15:17:15 +0900

    acceptance/departments/create_spec

--- spec/acceptance/departments/create_spec.rb
+++ spec/acceptance/departments/create_spec.rb
@@ -1,11 +1,39 @@
 require "rspec_api_documentation/dsl"
-require_dependency Rails.root.join("spec/acceptance/concerns/request_header_json")
+Rails.root.join("spec/acceptance/concerns").each_child { |path| require_dependency(path) }
 
 RSpec.resource "POST /departments", warden: true do
+  explanation "departmnets#create"
 
   include RequestHeaderJson
+  include OrganizationDataset
 
-  # POST   /departments
-  post "/departments" do
+  before do
+    login_as(admin_user)
+  end
+
+  post "/departments", focus: true do
+    with_options scope: :department do
+      parameter :name, "部署名", type: :string, required: true
+      parameter :owner_id, "部署責任者ID", type: :integer, required: true
+      parameter :department_id, "親部署ID", type: :integer
+    end
+
+    example "SUCCESS:" do
+      # NOTE
+      # admin_userしか部署は操作できない
+      # 同じOrganizationに属しているユーザをowner id に指定する
+      # department id も同じorganizationである必要がある
+
+      do_request(
+        department: {
+          name: "開発部",
+          owner_id: login_user.id,
+          department_id: nil
+        }
+      )
+
+      expect(status).to eq(201)
+      pp parse_response_body
+    end
   end
 end
