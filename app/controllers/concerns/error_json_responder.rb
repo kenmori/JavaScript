c6fbca6411729452a0041f54e3c2e6f12a615f2e
kbaba1001 <kbaba1001@users.noreply.github.com>
2018-11-15 17:51:04 +0900

    引数名が違うだけのメソッドなので削除

--- app/controllers/concerns/error_json_responder.rb
+++ app/controllers/concerns/error_json_responder.rb
@@ -30,20 +30,12 @@ module ErrorJsonResponder
 
     # render 422 with errors
     def unprocessable_entity_with_errors(errors)
-      render_with_errors(:unprocessable_entity, errors)
+      render_with_error(:unprocessable_entity, errors)
     end
 
     # render_with_error is render json of error.
     def render_with_error(code, message)
       # TODO: structureの定義を見直す
       render json: { error: message }, status: code
-    end
-
-    # render_with_errors is render json of ActiveModel::Errors.
-    def render_with_errors(code, errors)
-      # TODO: クライアントとどうエラーのやり取りを行うか考える
-      # errors.messageを展開するだけであれば展開した文字列を引数として
-      # 直接renderせずにrender_with_errorを呼び出す方式に変える
-      render json: { error: errors }, status: code
     end
 end
