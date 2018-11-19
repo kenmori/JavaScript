8bcdfc6a3bf263e0d7afc7126ffc5fee43c997b7
kbaba1001 <kbaba1001@users.noreply.github.com>
2018-11-19 23:48:37 +0900

    SUCCESS: delete key_result

--- spec/acceptance/key_results/destroy_spec.rb
+++ spec/acceptance/key_results/destroy_spec.rb
@@ -10,11 +10,23 @@ RSpec.resource "DELETE /key_results/:id", warden: true do
   include RequestHeaderJson
 
   before do
-    login_as(nomal_user)
+    login_as(admin_user)
   end
 
   delete "/key_results/:id" do
-    example "SUCCESS: "
-    example "ERROR: "
+    parameter :id, "削除対象とするKeyResultのID", type: :integer, required: true
+
+    example "SUCCESS: delete key_result" do
+      explanation "Objective責任者であればKeyResultを削除することができる"
+
+      do_request(id: key_result.id)
+
+      expect(response_status).to eq(200)
+      expect(parse_response_body("key_result", "id")).to eq(key_result.id)
+    end
+
+    example "ERROR: KeyResultがサインインユーザーとは別のOrganizationのとき"
+    example "ERROR: Objective責任者ではないとき"
+    example "ERROR: 何か削除できない時(unprocessable_entity_with_errorsにいくとき)"
   end
 end
