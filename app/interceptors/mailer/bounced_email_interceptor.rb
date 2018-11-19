ec8f991676af6a99d525b892ff6bcd21c841357c
neko-neko <seiichi.nishikata456@gmail.com>
2018-11-20 02:14:43 +0900

    Refactor

--- app/interceptors/mailer/bounced_email_interceptor.rb
+++ app/interceptors/mailer/bounced_email_interceptor.rb
@@ -5,13 +5,13 @@ class Mailer::BouncedEmailInterceptor
     message.to = reject_bounced_email(message.to)
     # CC/BCCは現状使っていないのでチェックしない
 
-    message.perform_deliveries = false if message.to.blank?
+    message.perform_deliveries = message.to.present?
   end
 
   private
 
     def self.reject_bounced_email(emails)
       bounced_emails = BounceEmail.where(email: emails).pluck(:email)
-      emails.reject { |e| bounced_emails.include?(e) }
+      emails - bounced_emails
     end
 end
