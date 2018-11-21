fc9b5839aee81bc95ded19ee5c5e5477b9fd8c75
kbaba1001 <kbaba1001@users.noreply.github.com>
2018-11-21 19:50:12 +0900

    SUCCESS: Make the specified KeyResult under the condition that the sign-in user has started

--- spec/acceptance/key_results/update_processed_spec.rb
+++ spec/acceptance/key_results/update_processed_spec.rb
@@ -4,7 +4,7 @@ require "rspec_api_documentation/dsl"
 Rails.root.join("spec/acceptance/concerns").each_child { |path| require_dependency(path) }
 
 RSpec.resource "PUT /key_results/:id/process", warden: true do
-  explanation "key_results#update_processed"
+  explanation "key_results#update_processed 指定したKeyResultをサインインユーザーが着手した状態にする"
 
   include OrganizationDataset
   include RequestHeaderJson
@@ -14,7 +14,25 @@ RSpec.resource "PUT /key_results/:id/process", warden: true do
   end
 
   put "/key_results/:id/process" do
-    example "SUCCESS: "
-    example "ERROR: "
+    parameter :id, "着手するKeyResultのID", type: :integer, required: true
+
+    example "SUCCESS: Make the specified KeyResult under the condition that the sign-in user has started" do
+      explanation "指定したKeyResultをサインインユーザーが着手した状態にする"
+
+      key_result.key_result_members.create(
+        user: nomal_user,
+        role: :member
+      )
+
+      do_request(
+        id: key_result.id
+      )
+
+      expect(response_status).to eq(204)
+      expect(response_body).to be_empty
+    end
+
+    example "ERROR: ログインユーザがKeyResultのメンバーでない場合エラー"
+    example "ERROR:"
   end
 end
