f0deeee83cc8fb95ef3f839e3e32f81bd6419ce9
kbaba1001 <kbaba1001@users.noreply.github.com>
2018-11-28 23:20:19 +0900

    ERROR: Can not archive if there is lower level departments

--- spec/acceptance/departments/destroy_spec.rb
+++ spec/acceptance/departments/destroy_spec.rb
@@ -22,5 +22,14 @@ RSpec.resource "DELETE /departments/:id", warden: true do
       expect(status).to eq(204)
       expect(parse_response_body).to be_blank
     end
+
+    example "ERROR: Can not archive if there is lower level departments" do
+      explanation "下位部署がある場合アーカイブ(無効化)できない"
+
+      do_request(id: dep_1_1.id)
+
+      expect(status).to eq(400)
+      expect(parse_response_error).to contain_exactly("下位部署が存在するのでアーカイブ出来ません")
+    end
   end
 end
