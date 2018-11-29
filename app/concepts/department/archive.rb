151b61dab88835cb1b18ec03e8fc4d78e96976b0
kbaba1001 <kbaba1001@users.noreply.github.com>
2018-11-29 15:26:38 +0900

    remove comments

--- app/concepts/department/archive.rb
+++ app/concepts/department/archive.rb
@@ -2,11 +2,6 @@ class Department::Archive < Trailblazer::Operation
   class Form < Reform::Form
     property :id
 
-    # ■無効化できる時の条件
-    # * [x] 下位部署がない
-    # * [x] その部署にユーザが属していない
-    # * [ ] 部署に紐づくOKRがある場合でも無効化できる
-
     validate -> {
       if model.has_children?
         errors.add(:base, :must_not_have_children)
@@ -26,16 +21,6 @@ class Department::Archive < Trailblazer::Operation
   step :delete_record
 
   def delete_record(options, model:, params:, **_metadata)
-    # :new: 管理者として、部署を削除 （無効化）したい
-    #
-    # 部署は削除は出来ず、削除とは無効化処理のことをさす。
-    # 現状、ユーザーを削除すると無効状態となる。これはプロフィール画像がグレーになって[無効]というバッチがつく。このような処理を部署でもやりたい。
-    # 無効化はアーカイブと捉えたほうが良さそう。
-    #
-    # ■他のアクションへの対応
-    # get系ではactiveかどうかを返す必要がある。
-    # activeな部署だけを返すような機能も必要かもしれない。scopeは少なくともいる
-
-    model.soft_destroy  # true/falseを返す
+    model.soft_destroy
   end
 end
