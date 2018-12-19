da488a1959c683bf996c28fe6b384e6acc017b33
Hiroaki Imai <hiro.1114.aki@gmail.com>
2018-12-20 03:33:29 +0900

    objective_comment_labels のデータ作成を rails runner から data-migrate に変更。

---
+++ db/data/20181219162700_add_objective_comment_label.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+class AddObjectiveCommentLabel < ActiveRecord::Migration[5.2]
+  using LoudApplicationRecord
+
+  def up
+    # 全組織に対して初期マスタレコードを追加する
+    ApplicationRecord.transaction do
+      Organization.all.each do |organization|
+        PresetObjectiveCommentLabels::Objective::DEFAULT_LABELS.each do |tag|
+          next if ObjectiveCommentLabel.exists?(name: tag[:name], color: tag[:color], organization: organization)
+          ObjectiveCommentLabel.create!(
+            name: tag[:name],
+            color: tag[:color],
+            organization: organization
+          )
+        end
+      end
+    end
+  end
+
+  def down
+    raise ActiveRecord::IrreversibleMigration
+  end
+end
