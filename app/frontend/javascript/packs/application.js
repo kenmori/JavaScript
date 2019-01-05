c75f250d4c6799e018837a572b5cf7d98daadc65
neko-neko <seiichi.nishikata456@gmail.com>
2019-01-05 22:32:05 +0900

    Remove why-did-you-update

--- app/frontend/javascript/packs/application.js
+++ app/frontend/javascript/packs/application.js
@@ -18,8 +18,4 @@ if (process.env.NODE_ENV !== 'production') {
   const Immutable = require('immutable')
   const installDevTools = require('immutable-devtools')
   installDevTools(Immutable)
-
-  const React = require('react')
-  const { whyDidYouUpdate } = require('why-did-you-update')
-  whyDidYouUpdate(React, { exclude: /./ }) // デフォルトでは対象から全コンポーネントを除外しておく
 }
