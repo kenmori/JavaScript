52dd050b56d577d1d2c27406392abfd214f548bd
kbaba1001 <kbaba1001@users.noreply.github.com>
2019-01-07 18:13:40 +0900

    Organizaitons/showで部署情報を取得できるようにした

--- app/views/organizations/show.jbuilder
+++ app/views/organizations/show.jbuilder
@@ -4,21 +4,21 @@ json.organization do
   json.partial! @organization
 
   json.okr_periods do
-    # TODO: partial! が激遅なのでインライン展開する
-    # json.partial! 'okr_periods/okr_period', collection: @organization.okr_periods, as: :okr_period
     json.array! @organization.okr_periods do |okr_period|
       json.extract! okr_period, :id, :name, :start_date, :end_date
     end
   end
 
   json.users do
-    # TODO: partial! が激遅なのでインライン展開する
-    # json.partial! 'users/user', collection: @organization.users, as: :user
     json.array! @organization.users do |user|
       json.extract! user, :id, :first_name, :last_name, :avatar_url, :disabled, :sign_in_at
       json.email user.unconfirmed_email || user.email
       json.is_confirming !user.confirmed? || user.unconfirmed_email
       json.is_admin user.admin?
+
+      json.departments do
+        json.partial! 'departments/department', collection: user.departments, as: :department
+      end
     end
   end
 end
---
+++ spec/acceptance/organizations/show_spec.rb
@@ -0,0 +1,59 @@
+require "rspec_api_documentation/dsl"
+Rails.root.join("spec/acceptance/concerns").each_child { |path| require_dependency(path) }
+
+RSpec.resource "GET /organizations", warden: true do
+  explanation "organizations#show"
+
+  include RequestHeaderJson
+  include DepartmentDataset
+
+  before do
+    login_as(admin_user)
+  end
+
+  get "/organizations/:id" do
+    parameter :id, "組織ID", type: :integer
+
+    example "SUCCESS: get a organization info" do
+      explanation "組織情報を取得する"
+
+      dep_1
+
+      do_request(
+        id: organization.id
+      )
+
+      expect(response_status).to eq(200)
+      expect(parse_response_body("organization")).to include(
+        "id" => organization.id,
+        "name" => "Test",
+        "logo" => {"url"=>nil},
+        "okr_span" => 3,
+        "okr_periods" => [],
+        "users" => [
+          {
+            "id" => admin_user.id,
+            "first_name"=> "太郎",
+            "last_name"=> "山田",
+            "avatar_url"=> nil,
+            "disabled"=> false,
+            "sign_in_at"=> be_time_iso8601,
+            "email"=>"yamada@example.com",
+            "is_confirming"=>nil,
+            "is_admin"=>true,
+            "departments" => [
+              {
+                "id" => dep_1.id,
+                "organization_id" => organization.id,
+                "display_order" => 1,
+                "name" => "代表",
+                "created_at" => be_time_iso8601,
+                "updated_at" => be_time_iso8601
+              }
+            ]
+          }
+        ]
+      )
+    end
+  end
+end
