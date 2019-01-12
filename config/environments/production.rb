6dacfbd8bfdc6a171ba4ec03679dcbe7c3da8d0c
neko-neko <seiichi.nishikata456@gmail.com>
2019-01-13 02:17:36 +0900

    Remove force_ssl in Rails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,11 +87,4 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
-  # SSL setting
-  config.force_ssl = true
-  config.ssl_options = {
-    hsts: { subdomains: true },
-    redirect: { exclude: ->(request) { request.headers[:HTTP_X_FORWARDED_FOR].blank? } }
-  }
 end
