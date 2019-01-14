6014c71e4804395a55ee665b3b23dd23b8a52d87
neko-neko <seiichi.nishikata456@gmail.com>
2019-01-14 21:04:51 +0900

    Improve nginx config

--- frontend/docker-entrypoint.sh
+++ frontend/docker-entrypoint.sh
@@ -1,6 +1,11 @@
 #!/bin/sh -eu
 
 # replace environment value
-sed -i "s/\${PROXY_PASS}/$PROXY_PASS/" /etc/nginx/nginx.conf
+sed -i "s/\${PROXY_PASS}/${PROXY_PASS:-api}/" /etc/nginx/nginx.conf
+if [[ ${FORCE_SECURE} = "true" ]]; then
+  sed -i "s/\${FORCE_SECURE}/add_header Strict-Transport-Security \"max-age=31536000\";/" /etc/nginx/nginx.conf
+else
+  sed -i "s/\${FORCE_SECURE}//" /etc/nginx/nginx.conf
+fi
 
 exec "$@"
