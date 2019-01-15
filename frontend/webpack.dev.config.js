75a793c9c8fd4c0896ab6517a02be23a0a2667d3
neko-neko <seiichi.nishikata456@gmail.com>
2019-01-15 16:27:29 +0900

    webpack.dev.configではprocess.envを見るように修正

--- frontend/webpack.dev.config.js
+++ frontend/webpack.dev.config.js
@@ -89,7 +89,7 @@ module.exports = {
     }),
     new webpack.DefinePlugin({
       "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV),
-      "process.env.GA_TRACKING_CODE": JSON.stringify("UA-111410984-4"),
+      "process.env.GA_TRACKING_CODE": JSON.stringify(process.env.GA_TRACKING_CODE),
     }),
     new webpack.HotModuleReplacementPlugin(),
   ],
