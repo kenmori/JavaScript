6c6458ba66a0705f857d93719f2dba5fb7b24be7
kbaba1001 <kbaba1001@users.noreply.github.com>
2019-01-21 14:09:03 +0900

    部署のjsonのレスポンスに関する部分をモデルに移動した

--- core-api/app/concepts/user/departments.rb
+++ core-api/app/concepts/user/departments.rb
@@ -7,18 +7,7 @@ class User::Departments < Trailblazer::Operation
 
   def query(options, model:, **_metadata)
     options[:query] = model.departments.arrange_serializable(order: :display_order) do |parent, children|
-      # TODO Department::Index とのコードの重複が気になる。モデルに移動する？
-      {
-        id: parent.id,
-        archived: parent.archived?,
-        soft_destroyed_at: parent.soft_destroyed_at,
-        name: parent.name,
-        display_order: parent.display_order,
-        created_at: parent.created_at,
-        updated_at: parent.updated_at,
-        user_count: parent.department_members.size,
-        children: children
-      }.with_indifferent_access
+      parent.serialize_index(children)
     end
   end
 end
--- core-api/app/models/department.rb
+++ core-api/app/models/department.rb
@@ -48,4 +48,46 @@ class Department < ApplicationRecord
   def first_root?
     kind == "first_root"
   end
+
+  # TODO OPTIMIZE Serialize 関係のメソッドが増えるなら ActiveRecord::Serializer の導入を検討する
+  def serialize_index(children)
+    {
+      id: id,
+      archived: archived?,
+      soft_destroyed_at: soft_destroyed_at,
+      name: name,
+      display_order: display_order,
+      created_at: created_at,
+      updated_at: updated_at,
+      user_count: department_members.size,
+      children: children
+    }.with_indifferent_access
+  end
+
+  def serialize_index_with_users(children)
+    {
+      id: id,
+      archived: archived?,
+      soft_destroyed_at: soft_destroyed_at,
+      name: name,
+      display_order: display_order,
+      created_at: created_at,
+      updated_at: updated_at,
+      user_count: department_members.size,
+      users: users.map do |user|
+        {
+          id: user.id,
+          first_name: user.first_name,
+          last_name: user.last_name,
+          avatar_url: user.avatar_url,
+          disabled: user.disabled,
+          sign_in_at: user.sign_in_at,
+          email: user.unconfirmed_email || user.email,
+          is_confirming: !user.confirmed? || user.unconfirmed_email,
+          is_admin: user.admin?
+        }
+      end,
+      children: children
+    }.with_indifferent_access
+  end
 end
