9d82217bd24c4d09555bb5300245dde457b9805d
neko-neko <seiichi.nishikata456@gmail.com>
2019-01-28 11:05:43 +0900

    HomeControllerを再追加した

---
+++ core-api/app/controllers/home_controller.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+class HomeController < ApplicationController
+  skip_before_action :authenticate_user!
+
+  def index; end
+end
--- core-api/config/routes.rb
+++ core-api/config/routes.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  root to: "home#index"
+
   # For rails monitor
   mount HealthMonitor::Engine, at: "/"
 
