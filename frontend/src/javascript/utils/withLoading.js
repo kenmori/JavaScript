098b102b3eb235b9b5c95823837d56307ccfa5bb
neko-neko <seiichi.nishikata456@gmail.com>
2019-02-01 13:24:15 +0900

    Fix withLoading

--- frontend/src/javascript/utils/withLoading.js
+++ frontend/src/javascript/utils/withLoading.js
@@ -1,38 +1,15 @@
-import { put, call, fork, select } from "redux-saga/effects";
+import { put } from "redux-saga/effects";
 import loadingActions from "../actions/loading";
 
-const TIME_TO_OPEN_LOADING = 200;
-let connectingCounter = 0;
+let actionCounter = 0;
 
-function wait(time) {
-  return () =>
-    new Promise(resolve => {
-      setTimeout(() => {
-        resolve();
-      }, time);
-    });
-}
-
-function* openLoading() {
-  yield call(wait(TIME_TO_OPEN_LOADING));
-  const state = yield select();
-  if (!state.loading.get("isForceCloseLoading") && connectingCounter) {
-    yield put(loadingActions.openLoading());
-  }
-}
-
-export default function withLoading(xhrRequest) {
+export default function withLoading(saga) {
   return function* () {
-    const state = yield select();
-    if (state.loading.get("isForceCloseLoading")) {
-      yield put(loadingActions.forceCloseLoadingOff());
+    if (++actionCounter === 1) {
+      yield put(loadingActions.openLoading());
     }
-
-    if (++connectingCounter === 1) {
-      yield fork(openLoading);
-    }
-    yield xhrRequest(...arguments);
-    if (--connectingCounter === 0) {
+    yield saga(...arguments);
+    if (--actionCounter === 0) {
       yield put(loadingActions.closeLoading());
     }
   };
