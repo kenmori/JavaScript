3fbd5d657ba99d37c3c70dd9c42e33ea7614e54c
neko-neko <seiichi.nishikata456@gmail.com>
2019-02-01 17:02:28 +0900

    Fix withLoading

--- frontend/src/javascript/utils/withLoading.js
+++ frontend/src/javascript/utils/withLoading.js
@@ -1,20 +1,14 @@
 import { put } from "redux-saga/effects";
 import loadingActions from "../actions/loading";
 
-let actionCounter = 0;
-
 export default function withLoading(saga) {
   return function* () {
-    if (++actionCounter === 1) {
-      yield put(loadingActions.openLoading());
-    }
+    yield put(loadingActions.openLoading());
     try {
       yield saga(...arguments);
     } catch (e) {
       // nothing to do
     }
-    if (--actionCounter === 0) {
-      yield put(loadingActions.closeLoading());
-    }
+    yield put(loadingActions.closeLoading());
   };
 }
