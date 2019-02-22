aa7edcef674d213661a401fb4aa5a59360f17384
neko-neko <seiichi.nishikata456@gmail.com>
2019-02-22 11:12:01 +0900

    Add slack description

--- frontend/src/javascript/components/organisms/AppSettingTab/index.jsx
+++ frontend/src/javascript/components/organisms/AppSettingTab/index.jsx
@@ -11,7 +11,10 @@ const AppSettingTab = React.memo(({ slackEnabled, onSegregate }) => (
     <Card className="app-settings-tab__card">
       <Image src={slack} className="app-settings-tab__card__logo" />
       <Card.Content>
-        <Card.Header>Slack</Card.Header>
+        <Card.Description>
+          <p>Slackと連携すると、Resilyの更新をSlack上で受け取る事が出来ます。</p>
+          <p><a href="https://resily.slack.com/apps/AGADQ1QSX-resily" target="_blank">詳細はこちら</a></p>
+        </Card.Description>
       </Card.Content>
       <Card.Content extra>
         <div className="ui two buttons">
