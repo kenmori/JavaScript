b66f12651dc19b09c0b5da90043b35e3b98fbb79
kenmori <kenjimoritata@gmail.com>
2023-04-22 14:27:38 +0900

    fix: remove no need file

--- js/150628_2.js
+++
@@ -1,9 +0,0 @@
-function MyClass = {};
-var Proto = {x:2,x:3};
-
-MyClass.prototype = Proto;
-var obj = new MyClass();
-
-//上と等価
-var Proto = {x:2,x:3};
-var obj = Object.create(Proto);
--- react0502/print.js
+++
@@ -1,3 +0,0 @@
-module.exports = function(msg){
-	document.write("[print]" + msg);
-};
